ff76f5bcc1e0650b2c020032eb27eb28ae788114
Peter Wittich <wittich@cornell.edu>
2018-03-15 06:06:39 -0400

    reformat loops for accurate trip count and optimization

--- GenericProject/OneToThree.cc
+++ GenericProject/OneToThree.cc
@@ -3,6 +3,7 @@
 #include "Constants.hh"
 
 
+//template<int par1, int par2, int par3>
 void OneToThree(
 	      const GenericData input[MAX_SIZE], // input
 	      const int n_in, // number of input data 
@@ -16,20 +17,27 @@ void OneToThree(
 //notes: * = deference pointer: value pointed to by
 //		 & = address of pointer: address of
 {
-//#pragma HLS pipeline II=1
+	const int par1 = 2;
+	const int par2 = 3;
+	const int par3 = 4;
 	n_out1 = 0;
 	n_out2 = 0;
 	n_out3 = 0;
-	LOOP_3_1: for(int i = 0; i < MAX_SIZE; ++i){
+	LOOP_1_3: for(int i = 0; i < MAX_SIZE; ++i){
 
-		if ( i == n_in) break;
-
-		out1[i] = 4 * input[i];
-		n_out1 += 1;
-		out2[i] = 2 * input[i];
-		n_out2 += 1;
-		out3[i] = 3 * input[i];
-		n_out3 += 1;
+		if ( i < n_in) {
+			out1[i] = par1 * input[i];
+			n_out1 += 1;
+			out2[i] = par2 * input[i];
+			n_out2 += 1;
+			out3[i] = par3 * input[i];
+			n_out3 += 1;
+		}
+		else {
+			out1[i] = 0;
+			out2[i] = 0;
+			out3[i] = 0;
+		}
 
 	}
 
