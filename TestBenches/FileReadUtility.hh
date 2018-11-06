e55c7d914938a4ca83e8db1cdaecee136a21856f
Zhengcheng Tao <zt75@cornell.edu>
2018-11-06 04:44:01 -0500

    Ensure FileReadUtility.hh used only in C simulation

--- TestBenches/FileReadUtility.hh
+++ TestBenches/FileReadUtility.hh
@@ -1,3 +1,6 @@
+// Utilities used only in test bench for C simulation
+#ifndef __SYNTHESIS__
+
 #ifndef FILEREADUTILITY
 #define FILEREADUTILITY
 
@@ -142,4 +145,6 @@ unsigned int compareMemWithFile(const MemType& memory, std::ifstream& fout,
   
 }
 
-#endif
+#endif  // FILEREADUTILITY
+
+#endif  // __SYNTHESIS__
