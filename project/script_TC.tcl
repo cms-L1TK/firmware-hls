ea9892b61a55c6b13737575321c04ab1382f7226
Andrew Hart <ahart@cern.ch>
2019-02-13 18:59:13 -0500

    Allow for the truncated or untruncated version to be tested.

--- project/script_TC.tcl
+++ project/script_TC.tcl
@@ -1,19 +1,23 @@
 open_project -reset trackletCalculator
 
-set_top TrackletCalculator_L1L2_5_8
+set_top TrackletCalculator_L1L2E
 add_files ../TrackletAlgorithm/TrackletCalculator.cpp -cflags "-std=c++11"
 add_files ../TrackletAlgorithm/TC_L1L2.cpp            -cflags "-std=c++11"
 add_files -tb ../TestBenches/TrackletCalculator_test.cpp -cflags "-I../TrackletAlgorithm -I../emData -std=c++11"
+add_files -tb ../emData/TC/tables
 add_files -tb ../emData/TC/TC_L1L2E
+add_files -tb ../emData/TC/TC_L1L2E_truncated
 
 open_solution -reset "solution_250MHz"
 set_part {xcku115-flvb2104-2-e} -tool vivado
 create_clock -period 4
 
-#csim_design -O -mflags "-j9 -k"
-#csim_design -mflags "-j9 -k"
+# Truncation: ON
+#csim_design -compiler gcc -O -mflags "-j9 -k"
+# Truncation: OFF
+#csim_design -compiler gcc -O -mflags "-j9 -k" -argv "0"
 #csynth_design
 #cosim_design
 #export_design -rtl verilog -format ip_catalog
 
-#exit
+exit
