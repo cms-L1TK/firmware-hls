f11924835207dd7180bd38483fde5f193aec677c
Derek Cranshaw <djc448@lnx231.classe.cornell.edu>
2020-02-19 13:14:10 -0500

    Updated latency pragmas in METopL3

--- TrackletAlgorithm/MatchEngineTopL3.cpp
+++ TrackletAlgorithm/MatchEngineTopL3.cpp
@@ -7,8 +7,8 @@ void MatchEngineTopL3(BXType bx,
 		      CandidateMatchMemory* outcandmatch){
 
 #pragma HLS interface register port=bx_o
-//#pragma HLS resource variable=instubdata latency=2
-//#pragma HLS resource variable=inprojdata latency=2
+//#pragma HLS resource variable=instubdata->get_mem() latency=2
+//#pragma HLS resource variable=inprojdata->get_mem() latency=2
 
   MatchEngine<3,BARRELPS>(bx,
 			  bx_o,
