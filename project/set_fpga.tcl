166166c930746341401d13d8c90c5f59c6a0e337
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2020-03-10 18:21:21 +0000

    Provide common settings for HLS and Vivado compilation (#65)
    * Moved clk frequency and FPGA type to settings_hls.tcl

--- project/set_fpga.tcl
+++
@@ -1,2 +0,0 @@
-set_part {xcvu7p-flvb2104-1-e} -tool vivado
-#set_part {xcku115-flvb2104-1-e} -tool vivado
---
+++ project/settings_hls.tcl
@@ -0,0 +1,20 @@
+# Set FPGA
+set_part {xcvu7p-flvb2104-1-e} -tool vivado
+#set_part {xcku115-flvb2104-1-e} -tool vivado
+
+# Set clock frequency
+create_clock -period 250MHz -name default
+
+# Allow HLS to use longer (easier to understand) names in resource/latency usage profiles.
+config_compile -name_max_length 100
+
+# Remove variables from IP interface if they are never used by HLS algo.
+config_interface -trim_dangling_port
+
+# Prevent HLS relaxing pipeline interval to meet timing.
+config_schedule -relax_ii_for_timing=0 -verbose
+
+# Encourage HLS to make more effort to find best solution.
+# (Worth trying, but increases CPU use, so not enabled by default)
+#config_bind -effort high
+#config_schedule -effort high -relax_ii_for_timing=0 -verbose
