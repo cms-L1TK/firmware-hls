f1a0e5f59793037a9f4425d56bf9c8cdbaf96fe7
Andrew Hart <ahart@cern.ch>
2020-03-23 12:15:21 -0400

    Read LUTs with a latency of two.

--- TrackletAlgorithm/TrackletEngineTop.cpp
+++ TrackletAlgorithm/TrackletEngineTop.cpp
@@ -10,6 +10,9 @@ void TrackletEngineTop(const BXType bx,
 {
 #pragma HLS resource variable=instubinnerdata.get_mem() latency=2
 #pragma HLS resource variable=instubouterdata.get_mem() latency=2
+#pragma HLS resource variable=pttable latency=2
+#pragma HLS resource variable=bendinnertable latency=2
+#pragma HLS resource variable=bendoutertable latency=2
   // !!! currently handles TE_L1PHIE18_L2PHIC17 only !!!
   TE_L1PHIE18_L2PHIC17: TrackletEngine<TE::L1L2, TE::E18, TE::C17, BARRELPS, BARRELPS, 32, 256, 256>
 			(bx, instubinnerdata, instubouterdata, pttable, bendinnertable, bendoutertable, outstubpair);
