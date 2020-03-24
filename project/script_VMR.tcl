465bf42114e70e1aea8aae8085dd75c66bd3de23
meholmbe <mei-li.holmberg@cern.ch>
2020-03-24 21:12:20 +0000

    made VMRouter templated, and removed VMRouter.cpp

--- project/script_VMR.tcl
+++ project/script_VMR.tcl
@@ -9,7 +9,7 @@ open_project -reset vmrouter
 # source files
 set CFLAGS {-std=c++11 -I../TrackletAlgorithm}
 set_top VMRouterTop
-add_files ../TrackletAlgorithm/VMRouter.cpp -cflags "$CFLAGS"
+#add_files ../TrackletAlgorithm/VMRouter.cpp -cflags "$CFLAGS"
 add_files ../TrackletAlgorithm/VMRouterTop.cc -cflags "$CFLAGS"
 add_files -tb ../TestBenches/VMRouter_test.cpp -cflags "$CFLAGS"
 
