32623307eecc37ce527c55c597b50fb782fe4880
meholmbe <mei-li.holmberg@cern.ch>
2020-04-20 14:04:39 +0100

    cleaned up code and added function to determine which bendcut table to use

--- TrackletAlgorithm/VMRouterTop.cc
+++ TrackletAlgorithm/VMRouterTop.cc
@@ -56,21 +56,26 @@ void VMRouterTop(BXType bx, const InputStubMemory<BARRELPS> *i0,
 #include "../emData/VMR/VMR_L1PHIE/VMR_L1PHIE_finebin.tab"
 	;
 
+	// Only used by layers. LUT with phi corrections for different r and bend
+	static const int phicorrtable[] =
+#include "../emData/VMR/VMPhiCorrL1.txt"
+	;
+
 	static const int binlookuptable[2048] = // 11 bits used for LUT
 #include "../emData/VMR/VMR_L1PHIE/VMTableInnerL1L2.tab" // Only for Layer 1
 					;
 
 
 // Bendcut tables
-static const int bendtablesize = 8;
+static const int bendtablesize = 8; // The size of each vmbendcut table. Either 8 or 16.
 static const int bendtable[][bendtablesize] = {
-			#include "../emData/VMR/VMSTE_L1PHIE17n1_vmbendcut.txt" // Seems to be the same for all E regions
-			// ,
-			// #include "../emData/VMR/VMSTE_L1PHIE17n1_vmbendcut.txt"
-			// ,
-			// #include "../emData/VMR/VMSTE_L1PHIE17n1_vmbendcut.txt"
-			// ,
-			// #include "../emData/VMR/VMSTE_L1PHIE17n1_vmbendcut.txt"
+			#include "../emData/VMR/VMSTE_L1PHIE17n1_vmbendcut.tab" // Seems to be the same for all E regions
+			,
+			#include "../emData/VMR/VMSTE_L1PHIE18n1_vmbendcut.tab"
+			,
+			#include "../emData/VMR/VMSTE_L1PHIE19n1_vmbendcut.tab"
+			,
+			#include "../emData/VMR/VMSTE_L1PHIE20n1_vmbendcut.tab"
 };
 
 // Overlap LUT
@@ -80,9 +85,8 @@ static const int bendtable[][bendtablesize] = {
 // SHOULD I USE SOMETHING ELSE THAN INT FOR MY TABLES???
 
 					VMRouter<BARRELPS, BARRELPS, layer, disk, bendtablesize>
-					//, ninputs, MEMask, TEIMask, OLMask, TEOMask>
-					(bx, finebintable, binlookuptable, bendtable, overlaptable, imask,
-					i0, i1, i2, i3, nullptr, nullptr, //i5,i6,i7,
+					(bx, finebintable, phicorrtable, binlookuptable, bendtable, overlaptable,
+					imask, i0, i1, i2, i3, nullptr, nullptr, //i5,i6,i7,
 					allStub,
 // ME memories
 					memask, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, // 0-7
