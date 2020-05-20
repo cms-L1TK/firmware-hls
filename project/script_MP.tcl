9e142f63b274845ec37a4aef99a9218d075c4d1e
Brent Yates <brentRyates@gmail.com>
2020-05-20 14:25:15 -0400

    Added tcl script

---
+++ project/script_MP.tcl
@@ -0,0 +1,33 @@
+# Script to generate project for PR
+#   vivado_hls -f script_PR.tcl
+#   vivado_hls -p match_processor
+# WARNING: this will wipe out the original project by the same name
+
+# create new project (deleting any existing one of same name)
+open_project -reset match_processor
+
+# source files
+set CFLAGS {-std=c++11 -I../TrackletAlgorithm}
+set_top MatchProcessorTopL3
+add_files ../TrackletAlgorithm/MatchProcessorTopL3.cpp -cflags "$CFLAGS"
+add_files -tb ../TestBenches/MatchProcessorL3_test.cpp -cflags "$CFLAGS"
+
+# data files
+add_files -tb ../emData/PR/
+add_files -tb ../emData/ME/
+add_files -tb ../emData/MC/
+add_files -tb ../emData/MP/
+
+open_solution "solution1"
+
+# Define FPGA, clock frequency & common HLS settings.
+source settings_hls.tcl
+
+csim_design -compiler gcc -mflags "-j8"
+csynth_design
+cosim_design 
+export_design -format ip_catalog
+# Adding "-flow impl" runs full Vivado implementation, providing accurate resource use numbers (very slow).
+#export_design -format ip_catalog -flow impl
+
+exit
