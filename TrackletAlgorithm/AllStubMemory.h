1f539bf9e5ab64b025cf566f82a82e435378f1c3
Andrew Hart <ahart@cern.ch>
2020-06-24 09:28:10 -0400

    Added a static_assert, since we have to assume DISK == 6.

--- TrackletAlgorithm/AllStubMemory.h
+++ TrackletAlgorithm/AllStubMemory.h
@@ -196,6 +196,7 @@ private:
 template<>
 class AllStub<6> : public AllStubBase<DISK> // Can't generate cosim files if AllStub<DISK> for some reason...
 {
+  static_assert(DISK == 6, "DISK is assumed to be 6 in this class specialization.");
 public:
   typedef ap_uint<AllStubBase<DISK>::kAllStubSize> AllStubData;
 
