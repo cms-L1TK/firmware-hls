6d8bd10146b720ef12b882405bbadf0379356b06
Brent Yates <brentRyates@gmail.com>
2020-10-07 11:44:44 -0400

    Switched to latest TVs

--- TrackletAlgorithm/MatchProcessor.h
+++ TrackletAlgorithm/MatchProcessor.h
@@ -243,7 +243,7 @@ void readTable_Cuts(ap_uint<width> table[depth]){
     }
     else if (L==3){
       ap_uint<width> tmp[depth] =
-#include "../emData/MC/MC_L3PHIC/MC_L3PHIC_phicut.tab"
+#include "../emData/MC/tables/MC_L3PHIC_phicut.tab"
       for (int i = 0; i < depth; i++) table[i] = tmp[i];
     }
     else if (L==4){
@@ -278,7 +278,7 @@ void readTable_Cuts(ap_uint<width> table[depth]){
     }
     else if (L==3){
       ap_uint<width> tmp[depth] =
-#include "../emData/MC/MC_L3PHIC/MC_L3PHIC_zcut.tab"
+#include "../emData/MC/tables/MC_L3PHIC_zcut.tab"
       for (int i = 0; i < depth; i++) table[i] = tmp[i];
     }
     else if (L==4){
