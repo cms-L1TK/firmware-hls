01c5f17e6748895fb8e156a0bc6f5488f5e16a86
Brent Yates <brentRyates@gmail.com>
2020-11-05 13:12:51 -0500

    Removed mask constants

--- TrackletAlgorithm/MatchCalculator_parameters.h
+++ TrackletAlgorithm/MatchCalculator_parameters.h
@@ -2,47 +2,15 @@
 #define TrackletAlgorithm_MatchCalculator_parameters_h
 
 // magic numbers for MC L3PHIC
-// shift and mask values shouldn't depend on layer/disk and phi sector
 
-// define masks
-static const auto mask_L1L2 = 1 << TF::L1L2;
-static const auto mask_L2L3 = 1 << TF::L2L3;
-static const auto mask_L3L4 = 1 << TF::L3L4;
-static const auto mask_L5L6 = 1 << TF::L5L6;
-static const auto mask_D1D2 = 1 << TF::D1D2;
-static const auto mask_D3D4 = 1 << TF::D3D4;
-static const auto mask_L1D1 = 1 << TF::L1D1;
-static const auto mask_L2D1 = 1 << TF::L2D1;
- 
 // masking out all but L1L2 and L5L6 seeding
 template<TF::layer Layer, MC::imc PHI> constexpr uint16_t FMMask() {
   return 0b00001001;
 }
 
 // return mask bit AND mask
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::L1L2>() {
-  return FMMask<TF::L3, MC::C>() & mask_L1L2;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::L2L3>() {
-  return FMMask<TF::L3, MC::C>() & mask_L2L3;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::L3L4>() {
-  return FMMask<TF::L3, MC::C>() & mask_L3L4;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::L5L6>() {
-  return FMMask<TF::L3, MC::C>() & mask_L5L6;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::D1D2>() {
-  return FMMask<TF::L3, MC::C>() & mask_D1D2;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::D3D4>() {
-  return FMMask<TF::L3, MC::C>() & mask_D3D4;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::L1D1>() {
-  return FMMask<TF::L3, MC::C>() & mask_L1D1;
-}
-template<> constexpr bool FMMask<TF::L3, MC::C, TF::L2D1>() {
-  return FMMask<TF::L3, MC::C>() & mask_L2D1;
+template<TF::layer Layer, MC::imc PHI, TF::seed Seed> constexpr bool FMMask() {
+  return FMMask<Layer, PHI>() & (1<<Seed);
 }
 
 #endif
