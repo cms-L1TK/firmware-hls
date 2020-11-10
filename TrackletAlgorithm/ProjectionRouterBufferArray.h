560fbff95a07fcf7c87babde3d0c63180cb53468
Brent Yates <brentRyates@gmail.com>
2020-11-10 15:02:24 -0500

    First third of Event 0 matches TVs
    Event: 0
    FullMatch:
    index   reference       computed
    0       0x14aa83ff002   0x14aa83ff002
    1       0x14ba8204002   0x14ba8204002
    2       0x187a6802001   0x187a6802001
    3       0x188a6804003   0x188a6804003
    4       0x20022a08000   0x20022a08000
    5       0x200a88041fd   missing
    6       0x20428402000   missing
    7       0x204a84091ff   missing
    8       0x241200031fd   missing
    9       0x246250011fe   missing
    a       0x24728bf5004   missing
    b       0x247a8bfa002   missing
    FullMatch:
    index   reference       computed
    0       0x0c80269ff007  missing
    1       0x0c81a00031ea  missing
    2       0x0c84a89ff1d7  missing
    3       0x0cc0284051c8  missing
    4       0x0cc0a2bfd17b  missing

---
+++ TrackletAlgorithm/ProjectionRouterBufferArray.h
@@ -0,0 +1,44 @@
+#ifndef PRBUFFERARRAY_HH
+#define PRBUFFERARRAY_HH
+
+#include "ProjectionRouterBuffer.h"
+
+template<int kNBitsBuffer> class ProjectionRouterBufferArray {
+public:
+  inline ProjectionRouterBuffer<BARREL> read() {
+    ap_uint<kNBitsBuffer> tmpptr = ptr_;
+    ptr_++;
+    //if(empty()) reset(); //read all projections, reset array to 0
+    //std::cout << std::hex << "reading proj=" << projbuffer[tmpptr].raw() << "\ttmpptr=" << tmpptr << "\tmoving ptr_=" << ptr_ << "\twidth_=" << width_ << std::endl;
+    return projbuffer[tmpptr];
+
+  }
+
+  inline void add(ProjectionRouterBuffer<BARREL> proj) {
+    projbuffer[width_] = proj;
+    //std::cout << std::hex << "adding proj=" << projbuffer[width_].raw() << "\twidth= " << width_ << std::endl;
+    width_++;
+  }
+
+  inline bool empty() { 
+    //std::cout << "Empty? " << (ptr_ == width_) << std::endl;
+    return ptr_ == width_;
+  }
+
+  inline void reset() {
+    ptr_ = 0;
+    width_ = 0;
+    //std::cout << "Resetting proj buffer array!" << std::endl;
+    empty();
+  }
+
+  ProjectionRouterBufferArray() { reset(); }
+
+private:
+  ap_uint<kNBitsBuffer> ptr_ = 0;
+  ap_uint<kNBitsBuffer> width_ = 0;
+  ProjectionRouterBuffer<BARREL> projbuffer[1<<kNBitsBuffer];
+
+};
+
+#endif
