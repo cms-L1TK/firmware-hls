c55e57146791f0d9b4a13ae56a28e902a02bfa54
Brent Yates <brentRyates@gmail.com>
2021-02-22 12:15:46 -0500

    Single projbufferarray
    MEU done once all nstubs are examined
    MP checks for idle/done tmp MEU first, then steps if available

--- TrackletAlgorithm/MatchEngineUnit.h
+++ TrackletAlgorithm/MatchEngineUnit.h
@@ -38,12 +38,18 @@ inline MatchEngineUnit() {
 #pragma HLS inline
   nstubs=0;
   ptr = 0;
+  unit_ = -1;
   idle_ = true;
   ready_ = false;
-  done_ = true;
+  done_ = false;
+  MEU_INIT: for(int i = 0; i < 1<<MatchEngineUnitBase<VMProjType>::kNBitsBuffer; ++i) {
+    #pragma HLS unroll
+    stubids[i] = 0;
+  }
 
 }
 
+/*
 inline MatchEngineUnit(const MatchEngineUnit& meu) {
 #pragma HLS inline
   writeindex = meu.writeindex;
@@ -57,12 +63,10 @@ inline MatchEngineUnit(const MatchEngineUnit& meu) {
   unit_ = meu.unit_;
   bx = meu.bx;
   istub = meu.istub;
-  /*
-  for(int i = 0; i < 1<<MatchEngineUnitBase<VMProjType>::kNBitsBuffer; ++i) {
+  MEU_INIT: for(int i = 0; i < 1<<MatchEngineUnitBase<VMProjType>::kNBitsBuffer; ++i) {
     #pragma HLS unroll
     stubids[i] = meu.stubids[i];
   }
-  */
   stubids = meu.stubids;
   projfinezadj = meu.projfinezadj;
   tcid = meu.tcid;
@@ -72,10 +76,45 @@ inline MatchEngineUnit(const MatchEngineUnit& meu) {
   projindex = meu.projindex;
   projbuffer = meu.projbuffer;
 }
+*/
+
+inline void operator=(const MatchEngineUnit& meu) {
+#pragma HLS inline
+  writeindex = meu.writeindex;
+  readindex = meu.readindex;
+  ptr = meu.ptr;
+  nstubs = meu.nstubs;
+  idle_ = meu.idle_;
+  done_ = meu.done_;
+  istep_ = meu.istep_;
+  ivmphi = meu.ivmphi;
+  unit_ = meu.unit_;
+  bx = meu.bx;
+  istub = meu.istub;
+  MEU_COPY: for(int i = 0; i < 1<<MatchEngineUnitBase<VMProjType>::kNBitsBuffer; ++i) {
+    #pragma HLS unroll
+    stubids[i] = meu.stubids[i];
+  }
+  /*
+  stubids = meu.stubids;
+  */
+  projfinezadj = meu.projfinezadj;
+  tcid = meu.tcid;
+  isPSseed = meu.isPSseed;
+  zbin = meu.zbin;
+  projrinv = meu.projrinv;
+  projindex = meu.projindex;
+  projbuffer = meu.projbuffer;
+  /*
+  if(copy_) { // Hack to only copy ptr if stepped (meutmp in MP.h)
+  }
+  */
+  copy_ = false;
+}
 
 inline void init(BXType bxin, ProjectionRouterBuffer<BARREL> projbuffer_, const INDEX iproj, int iphi, int unit) {
 #pragma HLS inline
-#pragma HLS ARRAY_PARTITION variable=stubids complete dim=1
+#pragma HLS ARRAY_PARTITION variable=stubids complete dim=0
 //#pragma HLS dependence variable=stubids inter false
 //#pragma HLS resource variable=stubids core=RAM_2P_LUTRAM
   writeindex = iproj;
@@ -85,14 +124,15 @@ inline void init(BXType bxin, ProjectionRouterBuffer<BARREL> projbuffer_, const 
   bx = bxin;
   projbuffer = projbuffer_;
   projindex = projbuffer_.getIndex();
+  nstubs=projbuffer.getNStubs();
   ivmphi = projbuffer_.getPhi();
   iphi_ = iphi;
   ptr = 0;
   unit_ = unit;
-  //std::cout << "Initializing MEU " << unit_ << std::endl;
+  std::cout << "Initializing MEU " << unit_ << std::endl;
   //print();
   //stubmem = stubmem_;
-  //std::cout << std::hex << "Initializing iphi=" << ivmphi << "\t" << projbuffer.raw() << " Received writeindex=" << writeindex << std::endl;
+  std::cout << std::hex << "Initializing iphi=" << ivmphi << "\t" << projbuffer.getProjection() << " Received writeindex=" << writeindex << std::endl;
   //projbuffer.Print();
 
 }
@@ -106,12 +146,14 @@ inline bool empty() {
 inline bool idle() {
 #pragma HLS inline  
   //std::cout << std::hex << "iphi=" << ivmphi << (idle_ ? "": " not") << " idle!" << std::endl;
+  //if(idle_) std::cout << "MEU " << unit_ << " idle" << std::endl;
   return idle_;
 }
 
 inline bool done() {
 #pragma HLS inline  
   //std::cout << projbuffer.raw() << " " << " iphi=" << ivmphi << (done_ ? "": " not") << " done!" << std::endl;
+  //if(done_) std::cout << "MEU " << unit_ << " done" << std::endl;
   return done_;
 }
 
@@ -164,8 +206,8 @@ inline void readNext(ProjectionRouterBuffer<BARREL>::TCID& trackletid, VMProject
   trackletid = getTCID();
   projid = getProjindex();
   stubid = stubids[ptr];
-  //std::cout << std::hex << "reading MEU " << unit_ << " " << projbuffer.getProjection() << "\tprojid=" << projbuffer.getIndex() << "\tstubid=" << stubid << "\t" << "iphi=" << ivmphi << "\treading=" << readindex << "\tptr=" << ptr << "\tmax=" << writeindex << std::endl;
   ptr++;
+  std::cout << std::hex << "reading MEU " << unit_ << " " << projbuffer.getProjection() << "\tprojid=" << projbuffer.getIndex() << "\tstubid=" << stubid << "\t" << "iphi=" << ivmphi << "\treading=" << readindex << "\tptr=" << ptr << "\tmax=" << writeindex << std::endl;
   idle_ = ptr >= readindex ? true : idle_;
   //idle_ = ready() ? idle_ : true;
 
@@ -176,9 +218,9 @@ void print() {
   if(empty()) 
     std::cout << "Empty MEU " << unit_ << " projid=" << projbuffer.getIndex() << std::endl;
   else {
-    std::cout << "Unread contents in MEU "  << unit_ << " projid=" << projbuffer.getIndex() << std::endl;
+    std::cout << "Unread contents in MEU "  << unit_ << " projid=" << projbuffer.getIndex() << " " << projbuffer.getProjection() << std::endl;
     for(int i = ptr; i < readindex; ++i){
-      std::cout << std::hex << i << ": " << stubids[i] << std::endl;
+      std::cout << std::hex << i << ": " << stubids[i] << (i==ptr ? " <=== ptr" : "" ) << std::endl;
     }
   }
 }
@@ -188,8 +230,10 @@ void print() {
 inline bool step(bool *table, const VMStubMEMemoryCM<VMSMEType,3,3> &stubmem) {
 #pragma HLS inline
 //#pragma HLS dependence variable=istub inter WAR true
-    //std::cout << "step " << projbuffer.raw() << "\t" << "iphi=" << ivmphi << "\treading=" << readindex << "\tmax=" << writeindex << std::endl;
+#pragma HLS dependence variable=idle_ intra RAW true
     if(idle() || done()) return true;
+    copy_ = true;
+    std::cout << "step " << projbuffer.getProjection() << "\t" << "iphi=" << ivmphi << "\treading=" << readindex << "\tmax=" << nstubs << std::endl;
 
     ////////////////////////////////////////////
     //This seems like where the ME buffer starts
@@ -242,6 +286,7 @@ inline bool step(bool *table, const VMStubMEMemoryCM<VMSMEType,3,3> &stubmem) {
         if (nstubs==1) {
           istub=0;
           readindex++;
+          done_ = true;
         } else {
           istub++;
         }
@@ -250,6 +295,7 @@ inline bool step(bool *table, const VMStubMEMemoryCM<VMSMEType,3,3> &stubmem) {
         if (istub+1>=nstubs){
           istub=0;
           readindex++;
+          done_ = true;
         } else {
           istub++;
         }
@@ -258,9 +304,11 @@ inline bool step(bool *table, const VMStubMEMemoryCM<VMSMEType,3,3> &stubmem) {
       //Read stub memory and extract data fields
       //auto const  stubadd=zbin.concat(istubtmp);
       int stubadd=16*(iphi_*8+zbin)+istubtmp;
+      std::cout << "reading istubtmp=" << istubtmp << " stubadd=" << stubadd << std::endl;
       const VMStubMECM<VMSMEType> stubdata=stubmem.read_mem(bx,stubadd);
       //const VMStubMECM<VMSMEType> stubdata=stubmem[unit_].read_mem(bx,stubadd);
       auto stubindex=stubdata.getIndex();
+      std::cout << "stubid=" << stubindex << std::endl;
       auto stubfinez=stubdata.getFineZ();
       auto stubbend=stubdata.getBend();
 
@@ -277,13 +325,16 @@ inline bool step(bool *table, const VMStubMEMemoryCM<VMSMEType,3,3> &stubmem) {
       auto const index=projrinv.concat(stubbend);
       if (pass&&table[index]) {
       stubids[readindextmp]=stubindex;
-      //std::cout << std::hex << "MEU found stubid=" << stubindex << std::endl;
+      std::cout << std::hex << "MEU found stubid=" << stubindex << std::endl;
     } // if(pass&&table[index])
+    print();
     //if(istub==0 && nstubs>0) idle_ = true;
     ready_ = ptr < readindex;
-    if(readindex>nstubs) done_ = true;
+    if(readindex>=nstubs) done_ = true;
+    std::cout << "readindex=" << readindex << "\tnstubs=" << nstubs << "\tdone=" << done() << std::endl;
     //if(readindex>writeindex) done_ = true;
     //if(istub==0 && nstubs>0) return true;
+    //if(done_) std::cout << "MEU " << unit_ << "done stepping" << std::endl;
     if(done_) return true;
  
     //-----------------------------------------------------------------------------------------------------------
@@ -305,6 +356,7 @@ inline bool step(bool *table, const VMStubMEMemoryCM<VMSMEType,3,3> &stubmem) {
   bool idle_;
   bool ready_;
   bool done_;
+  bool copy_;
   int istep_;
   int ivmphi;
   int iphi_;
