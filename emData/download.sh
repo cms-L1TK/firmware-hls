012352fd6b2e200c16e0c812d28a8f958f09e367
Andrew Hart <ahart@cern.ch>
2021-03-15 20:49:10 -0400

    Removed supplemental PD tarball.

--- emData/download.sh
+++ emData/download.sh
@@ -8,11 +8,6 @@
 tarball_url="https://www.dropbox.com/s/8942mkttj198y90/MemPrintsStandard_210315.tgz?dl=0"
 luts_url="https://www.dropbox.com/s/6ebazqxtb1e0caw/LUTsStandard_210315.tgz?dl=0"
 
-# Supplemental test-vectors for development of the PurgeDuplicates module.
-# Generated after PR #70 was merged:
-# https://github.com/cms-L1TK/cmssw/tree/1606921d6c26b13292808ec8198db8c6a400dad0
-pd_url="https://cernbox.cern.ch/index.php/s/tk6d4GPnMlMKZNf/download"
-
 # fw_synch_200515
 #memprints_url="https://cernbox.cern.ch/index.php/s/QvV86Qcc8n9R4sg/download"
 #luts_url="https://cernbox.cern.ch/index.php/s/YSER9ne7WVxiKXI/download"
@@ -68,6 +63,9 @@ declare -a processing_modules=(
   "MC_L4PHIC"
   "MC_L5PHIC"
   "MC_L6PHIC"
+
+  # TrackBuilder (aka FitTrack)
+  "FT_L1L2"
 
   # Tracklet Processor
   "TP_L1L2D" 
@@ -152,11 +150,6 @@ rm -f MemPrints.tgz
 wget -O MemPrints.tar.gz --quiet ${tarball_url}
 tar -xzf MemPrints.tar.gz
 rm -f MemPrints.tar.gz
-
-# Download and unpack PD.tar.gz
-wget -O PD.tar.gz --quiet ${pd_url}
-tar -xzf PD.tar.gz
-rm -f PD.tar.gz
 
 # Needed in order for awk to run successfully:
 # https://forums.xilinx.com/t5/Installation-and-Licensing/Vivado-2016-4-on-Ubuntu-16-04-LTS-quot-awk-symbol-lookup-error/td-p/747165
