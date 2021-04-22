64759690b1e1f1e2c72843f5502459e4b987e719
Rui Zou <rz393@cornell.edu>
2021-04-22 12:26:19 -0400

    fix typo in tf_lut.vhd

--- IntegrationTests/common/hdl/tf_lut.vhd
+++ IntegrationTests/common/hdl/tf_lut.vhd
@@ -34,7 +34,7 @@ architecture behavioral of tf_lut is
       hread(data_fileLine, ROM(I));
     end loop;
     return ROM;
-  end initRomFromFile;
+  end function;
   signal rom : romType := initRomFromFile;
 
   attribute syn_rom_style : string;
