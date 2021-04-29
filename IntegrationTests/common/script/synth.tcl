36b1e0c951cfb5e7d1bb46262bddff45795b26e1
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2021-04-29 14:50:18 +0100

    Added opt options for synthesis

--- IntegrationTests/common/script/synth.tcl
+++ IntegrationTests/common/script/synth.tcl
@@ -11,11 +11,23 @@ open_project ./${_xil_proj_name_}/${_xil_proj_name_}.xpr
 
 reset_run synth_1
 
+# Best strategy to meet timing at high frequency (but slows compilation).
+#set_property "strategy" "Flow_AlternateRoutability" [get_runs synth_1]
+#set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
+#set_property STEPS.SYNTH_DESIGN.ARGS.NO_LC false [get_runs synth_1]
+#set_property STEPS.SYNTH_DESIGN.ARGS.SHREG_MIN_SIZE 3 [get_runs synth_1]
+
+# Needed if design doesn't connect to I/O pins of FPGA.
+set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
+
 # Synthesis
 update_compile_order -fileset sources_1
 launch_runs synth_1 -jobs 4
 wait_on_run synth_1
+
+# Make reports
 open_run synth_1
-report_utilization -hierarchical -file report_utilization_hierarchical.txt
+report_timing_summary -file timing_synth_1.rpt
+report_utilization -hierarchical -file utilization_synth.rpt
 
 exit
