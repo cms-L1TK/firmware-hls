e7fd6f4a6c22568ea1c139c19afad6b686f94133
kwalkingshaw <katie.walkingshawpass@bristol.ac.uk>
2021-05-04 15:30:24 +0100

    Pushing work to main repo

--- TrackletAlgorithm/TrackMergerTop.cc
+++ TrackletAlgorithm/TrackMergerTop.cc
@@ -1,5 +1,6 @@
 #include "TrackMergerTop.h"
 
-void TrackMergerTop(const BXType bx, TrackFitMemory inputTracks, TrackFitMemory &outputTracks){
+void TrackMergerTop(const BXType bx, TrackFit::Trackword trackWord [kMaxProc], TrackFit::BarrelStubWord barrelStubWords[4][kMaxProc], 
+TrackFit::DiskStubWord diskStubWords[4][kMaxProc])){
     TrackMerger(bx, inputTracks, outputTracks);
 }
