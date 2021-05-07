909078d1c8c4058371a7618fc52273fd2cb3a8da
kwalkingshaw <katie.walkingshawpass@bristol.ac.uk>
2021-05-07 14:45:16 +0100

    Pushing changes before rebasing

--- TrackletAlgorithm/TrackMergerTop.h
+++ TrackletAlgorithm/TrackMergerTop.h
@@ -3,7 +3,13 @@
 
 #include "TrackMerger.h"
 
-void TrackMergerTop(const BXType bx, TrackFit::TrackWord trackWord [kMaxProc], TrackFit::BarrelStubWord barrelStubWords[4][kMaxProc], 
-TrackFit::DiskStubWord diskStubWords[4][kMaxProc]);
+void TrackMergerTop(const BXType bx,
+  const TrackFit::TrackWord trackWord [kMaxProc],
+  const TrackFit::BarrelStubWord barrelStubWords[4][kMaxProc],
+  const TrackFit::DiskStubWord diskStubWords[4][kMaxProc],
+  BXType &bx_o,
+  TrackFit::TrackWord trackWord_o [kMaxProc],
+  TrackFit::BarrelStubWord barrelStubWords_o[4][kMaxProc],
+  TrackFit::DiskStubWord diskStubWords_o[4][kMaxProc]);
 
 #endif
