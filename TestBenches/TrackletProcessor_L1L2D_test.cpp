07046c8c916addb90e1809bac1a64f4e58d87cf9
Andrew Hart <ahart@cern.ch>
2021-05-26 09:18:27 -0400

    Fixed bugs in TC and TP test benches.

--- TestBenches/TrackletProcessor_L1L2D_test.cpp
+++ TestBenches/TrackletProcessor_L1L2D_test.cpp
@@ -217,8 +217,8 @@ int main()
 
   } // end of event loop
 
-  cout << "Number of errors : "<<err<<endl;
-  //return err;
-  return 0;
+  // This is necessary because HLS seems to only return an 8-bit error count, so if err%256==0, the test bench can falsely pass
+  if (err > 255) err = 255;
+  return err;
 
 }
