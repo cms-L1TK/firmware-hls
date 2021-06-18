083d83f018d5ebea0184c2f8d489be9caef4be45
meholmbe <mei-li.holmberg@cern.ch>
2021-06-18 11:07:33 +0100

    added new memUtil_pkg.vhd

--- IntegrationTests/IRVMR/hdl/memUtil_pkg.vhd
+++ IntegrationTests/IRVMR/hdl/memUtil_pkg.vhd
@@ -42,41 +42,149 @@ package memUtil_pkg is
   type t_arr_VMSTE_22_ADDR is array(enum_VMSTE_22) of std_logic_vector(7 downto 0);
   type t_arr_VMSTE_22_DATA is array(enum_VMSTE_22) of std_logic_vector(21 downto 0);
   type t_arr_VMSTE_22_NENT is array(enum_VMSTE_22) of t_arr2_7b;
-  
-  -- ########################### Functions ########################
-  
-  function get_filename(val: enum_DL_39) return string;
-  function get_filename(val: enum_AS_36) return string;
+
+  -- ########################### Functions ###########################
+
+  -- Following functions are needed because VHDL is not case sensitive when converting an enum to a string
+  function memory_enum_to_string(val: enum_DL_39) return string;
+  function memory_enum_to_string(val: enum_IL_36) return string;
+  function memory_enum_to_string(val: enum_AS_36) return string;
+  function memory_enum_to_string(val: enum_VMSME_16) return string;
+  function memory_enum_to_string(val: enum_VMSTE_16) return string;
+  function memory_enum_to_string(val: enum_VMSTE_22) return string;
 
 end package memUtil_pkg;
 
-
 package body memUtil_pkg is
-  
-  -- ########################### Functions ########################
 
-  function get_filename(val: enum_DL_39) return string is
+  -- ########################### Functions ###########################
+
+  function memory_enum_to_string(val: enum_DL_39) return string is
   begin
     case val is
-      when PS10G_3_A => return "Link_DL_PS10G_3_A_04.dat";
-      when negPS10G_3_A => return "Link_DL_negPS10G_3_A_04.dat";
+       when PS10G_3_A => return "PS10G_3_A";
+       when negPS10G_3_A => return "negPS10G_3_A";
     end case;
-    return "No filename found.";
-  end get_filename;
+    return "No conversion found.";
+  end memory_enum_to_string;
 
-  function get_filename(val: enum_AS_36) return string is
+  function memory_enum_to_string(val: enum_IL_36) return string is
   begin
     case val is
-      when L2PHIAn1 => return "AllStubs_AS_L2PHIAn1_04.dat";
-      when L2PHIAn2 => return "AllStubs_AS_L2PHIAn2_04.dat";
-      when L2PHIAn3 => return "AllStubs_AS_L2PHIAn3_04.dat";
-      when L2PHIAn4 => return "AllStubs_AS_L2PHIAn4_04.dat";
-      when L2PHIAn5 => return "AllStubs_AS_L2PHIAn5_04.dat";
-      when L2PHIAn6 => return "AllStubs_AS_L2PHIAn6_04.dat"; 
-      when L2PHIAn7 => return "AllStubs_AS_L2PHIAn7_04.dat";
+       when L2PHIB_PS10G_3_A => return "L2PHIB_PS10G_3_A";
+       when L2PHIC_PS10G_3_A => return "L2PHIC_PS10G_3_A";
+       when D2PHIA_PS10G_3_A => return "D2PHIA_PS10G_3_A";
+       when D2PHIB_PS10G_3_A => return "D2PHIB_PS10G_3_A";
+       when D2PHIC_PS10G_3_A => return "D2PHIC_PS10G_3_A";
+       when D2PHID_PS10G_3_A => return "D2PHID_PS10G_3_A";
+       when L2PHIB_negPS10G_3_A => return "L2PHIB_negPS10G_3_A";
+       when L2PHIC_negPS10G_3_A => return "L2PHIC_negPS10G_3_A";
+       when D2PHIA_negPS10G_3_A => return "D2PHIA_negPS10G_3_A";
+       when D2PHIB_negPS10G_3_A => return "D2PHIB_negPS10G_3_A";
+       when D2PHIC_negPS10G_3_A => return "D2PHIC_negPS10G_3_A";
+       when D2PHID_negPS10G_3_A => return "D2PHID_negPS10G_3_A";
+       when L2PHIA_PS10G_3_A => return "L2PHIA_PS10G_3_A";
+       when L2PHIA_negPS10G_3_A => return "L2PHIA_negPS10G_3_A";
     end case;
-    return "No filename found.";
-  end get_filename;
+    return "No conversion found.";
+  end memory_enum_to_string;
+
+  function memory_enum_to_string(val: enum_AS_36) return string is
+  begin
+    case val is
+       when L2PHIAn1 => return "L2PHIAn1";
+       when L2PHIAn2 => return "L2PHIAn2";
+       when L2PHIAn3 => return "L2PHIAn3";
+       when L2PHIAn4 => return "L2PHIAn4";
+       when L2PHIAn5 => return "L2PHIAn5";
+       when L2PHIAn6 => return "L2PHIAn6";
+       when L2PHIAn7 => return "L2PHIAn7";
+    end case;
+    return "No conversion found.";
+  end memory_enum_to_string;
+
+  function memory_enum_to_string(val: enum_VMSME_16) return string is
+  begin
+    case val is
+       when L2PHIA1n1 => return "L2PHIA1n1";
+       when L2PHIA2n1 => return "L2PHIA2n1";
+       when L2PHIA3n1 => return "L2PHIA3n1";
+       when L2PHIA4n1 => return "L2PHIA4n1";
+       when L2PHIA5n1 => return "L2PHIA5n1";
+       when L2PHIA6n1 => return "L2PHIA6n1";
+       when L2PHIA7n1 => return "L2PHIA7n1";
+       when L2PHIA8n1 => return "L2PHIA8n1";
+    end case;
+    return "No conversion found.";
+  end memory_enum_to_string;
+
+  function memory_enum_to_string(val: enum_VMSTE_16) return string is
+  begin
+    case val is
+       when L2PHIA1n1 => return "L2PHIA1n1";
+       when L2PHIA1n2 => return "L2PHIA1n2";
+       when L2PHIA1n3 => return "L2PHIA1n3";
+       when L2PHIA2n1 => return "L2PHIA2n1";
+       when L2PHIA2n2 => return "L2PHIA2n2";
+       when L2PHIA2n3 => return "L2PHIA2n3";
+       when L2PHIA2n4 => return "L2PHIA2n4";
+       when L2PHIA3n1 => return "L2PHIA3n1";
+       when L2PHIA3n2 => return "L2PHIA3n2";
+       when L2PHIA3n3 => return "L2PHIA3n3";
+       when L2PHIA3n4 => return "L2PHIA3n4";
+       when L2PHIA3n5 => return "L2PHIA3n5";
+       when L2PHIA4n1 => return "L2PHIA4n1";
+       when L2PHIA4n2 => return "L2PHIA4n2";
+       when L2PHIA4n3 => return "L2PHIA4n3";
+       when L2PHIA4n4 => return "L2PHIA4n4";
+       when L2PHIA4n5 => return "L2PHIA4n5";
+       when L2PHIA5n1 => return "L2PHIA5n1";
+       when L2PHIA5n2 => return "L2PHIA5n2";
+       when L2PHIA5n3 => return "L2PHIA5n3";
+       when L2PHIA5n4 => return "L2PHIA5n4";
+       when L2PHIA5n5 => return "L2PHIA5n5";
+       when L2PHIA6n1 => return "L2PHIA6n1";
+       when L2PHIA6n2 => return "L2PHIA6n2";
+       when L2PHIA6n3 => return "L2PHIA6n3";
+       when L2PHIA6n4 => return "L2PHIA6n4";
+       when L2PHIA6n5 => return "L2PHIA6n5";
+       when L2PHIA7n1 => return "L2PHIA7n1";
+       when L2PHIA7n2 => return "L2PHIA7n2";
+       when L2PHIA7n3 => return "L2PHIA7n3";
+       when L2PHIA7n4 => return "L2PHIA7n4";
+       when L2PHIA7n5 => return "L2PHIA7n5";
+       when L2PHIA8n1 => return "L2PHIA8n1";
+       when L2PHIA8n2 => return "L2PHIA8n2";
+       when L2PHIA8n3 => return "L2PHIA8n3";
+       when L2PHIA8n4 => return "L2PHIA8n4";
+       when L2PHIA8n5 => return "L2PHIA8n5";
+    end case;
+    return "No conversion found.";
+  end memory_enum_to_string;
+
+  function memory_enum_to_string(val: enum_VMSTE_22) return string is
+  begin
+    case val is
+       when L2PHII1n1 => return "L2PHII1n1";
+       when L2PHII1n2 => return "L2PHII1n2";
+       when L2PHII2n1 => return "L2PHII2n1";
+       when L2PHII2n2 => return "L2PHII2n2";
+       when L2PHII2n3 => return "L2PHII2n3";
+       when L2PHII3n1 => return "L2PHII3n1";
+       when L2PHII3n2 => return "L2PHII3n2";
+       when L2PHII3n3 => return "L2PHII3n3";
+       when L2PHII4n1 => return "L2PHII4n1";
+       when L2PHII4n2 => return "L2PHII4n2";
+       when L2PHII4n3 => return "L2PHII4n3";
+       when L2PHIX1n5 => return "L2PHIX1n5";
+       when L2PHIX1n6 => return "L2PHIX1n6";
+       when L2PHIX1n7 => return "L2PHIX1n7";
+       when L2PHIX2n5 => return "L2PHIX2n5";
+       when L2PHIX2n6 => return "L2PHIX2n6";
+       when L2PHIX2n7 => return "L2PHIX2n7";
+       when L2PHIX2n8 => return "L2PHIX2n8";
+    end case;
+    return "No conversion found.";
+  end memory_enum_to_string;
 
 end package body memUtil_pkg;
-      
