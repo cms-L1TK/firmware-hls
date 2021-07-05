a6dd190526ce3360e8c8d55610d302beb196b9c8
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2021-07-05 15:08:24 +0100

    fix txt file read

--- IntegrationTests/common/hdl/tf_lut.vhd
+++ IntegrationTests/common/hdl/tf_lut.vhd
@@ -7,18 +7,19 @@
 library ieee ;
 use ieee.std_logic_1164.all;
 use ieee.numeric_Std.all;
-library std ;
+use ieee.math_real.all;
+library std;
 use std.textio.all;
 
 use work.tf_pkg.all;
 
 entity tf_lut is
-  generic (lut_file : string := "lut.dat";
-           lut_width   : integer := 1;
-           lut_depth    : integer := 256);
+  generic (lut_file  : string := "lut.dat";
+           lut_width : integer := 1;
+           lut_depth : integer := 256);
   port (
-    clk : in std_logic;
-    ce : in std_logic;
+    clk  : in std_logic;
+    ce   : in std_logic;
     addr : in std_logic_vector(clogb2(lut_depth)-1 downto 0);
     dout : out std_logic_vector(lut_width-1 downto 0)
     );
@@ -30,31 +31,23 @@ architecture behavioral of tf_lut is
   impure function initRomFromFile return romType is
     file data_file : text open read_mode is lut_file;
     variable line_in : line;
-    variable line_data : std_logic_vector(127 downto 0) := (others => '0'); -- A wide vector compared to any LUT width.
-    variable hasComma : boolean := false;
-    variable nCharSkip : natural := 0;
+    -- Determines number of 4*chars needed to be read from lut_file
+    -- to correspond to specified LUT width.
+    constant lut_width_4 : integer := 4*integer(ceil(real(lut_width)/4));
+    variable line_data : std_logic_vector(lut_width_4-1 downto 0) := (others => '0'); 
     variable rom_data : romType;
-    variable addr : natural := 0;
   begin
-    while not endfile(data_file) loop
+    -- N.B. line_in.all is not supported by synthesis.
+
+    -- Skip first line (just a bracket).
+    readline(data_file, line_in);
+
+    for I in romType'range loop
       readline(data_file, line_in);
-      -- Skip lines without hex numbers
-      if (line_in'length >= 1) then
-        -- Skip brackets at start & end of file.
-        if (line_in.all(1 to 1) /= "{" and line_in.all(1 to 1) /= "}") then
-          -- Check if line ends in comma, in which case drop comma.
-          hasComma := (line_in.all(line_in'length to line_in'length) = ",");
-          if hasComma then
-            nCharSkip := 1;
-          else
-            nCharSkip := 0;
-          end if;
-          hread(line_in, line_data((line_in'length-nCharSkip)*4-1 downto 0));
-          -- Truncate data word to desired width.
-          rom_data(addr) := line_data(lut_width-1 downto 0);
-          addr := addr + 1;
-        end if;
-      end if;
+      -- This avoid reading the final comma that appears on some lines.
+      hread(line_in, line_data);
+      -- Truncate data word to desired width.
+      rom_data(I) := line_data(lut_width-1 downto 0);
     end loop;
     return rom_data;
   end function;
