84a12c0e3903123cd2b333d63bbd9532e2d113c4
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2021-07-05 02:20:27 +0100

    bug fix

--- IntegrationTests/common/hdl/tf_lut.vhd
+++ IntegrationTests/common/hdl/tf_lut.vhd
@@ -26,14 +26,35 @@ end entity tf_lut;
 
 architecture behavioral of tf_lut is
   type romType is array(0 to lut_depth-1) of std_logic_vector(lut_width-1 downto 0);
+
   impure function initRomFromFile return romType is
     file data_file : text open read_mode is lut_file;
-    variable data_fileLine : line;
+    variable line_in : line;
+    variable line_data : std_logic_vector(127 downto 0) := (others => '0'); -- A wide vector compared to any LUT width.
+    variable hasComma : boolean := false;
+    variable nCharSkip : natural := 0;
     variable rom_data : romType;
+    variable addr : natural := 0;
   begin
-    for I in romType'range loop
-      readline(data_file, data_fileLine);
-      hread(data_fileLine, rom_data(I));
+    while not endfile(data_file) loop
+      readline(data_file, line_in);
+      -- Skip lines without hex numbers
+      if (line_in'length >= 1) then
+        -- Skip brackets at start & end of file.
+        if (line_in.all(1 to 1) /= "{" and line_in.all(1 to 1) /= "}") then
+          -- Check if line ends in comma, in which case drop comma.
+          hasComma := (line_in.all(line_in'length to line_in'length) = ",");
+          if hasComma then
+            nCharSkip := 1;
+          else
+            nCharSkip := 0;
+          end if;
+          hread(line_in, line_data((line_in'length-nCharSkip)*4-1 downto 0));
+          -- Truncate data word to desired width.
+          rom_data(addr) := line_data(lut_width-1 downto 0);
+          addr := addr + 1;
+        end if;
+      end if;
     end loop;
     return rom_data;
   end function;
