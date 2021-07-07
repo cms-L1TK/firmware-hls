ba5b601952e41b5e01550502bcdc24d9f09a9f22
Brent Yates <brentRyates@gmail.com>
2021-07-07 09:01:59 -0400

    Generated top level files now run

--- TrackletAlgorithm/MatchCalculatorTop.cc
+++ TrackletAlgorithm/MatchCalculatorTop.cc
@@ -1,35 +1,41 @@
 #include "MatchCalculatorTop.h"
 
-void MatchCalculatorTop(BXType bx,
-                        const CandidateMatchMemory match[maxMatchCopies],
-                        const AllStubMemory<BARRELPS>* allstub,
-                        const AllProjectionMemory<BARRELPS>* allproj,
-                        BXType& bx_o,
-                        FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
-                        )
-{
- #pragma HLS inline off
- #pragma HLS interface register port=bx_o
- #pragma HLS resource variable=match[0].get_mem() latency=2
- #pragma HLS resource variable=match[1].get_mem() latency=2
- #pragma HLS resource variable=match[2].get_mem() latency=2
- #pragma HLS resource variable=match[3].get_mem() latency=2
- #pragma HLS resource variable=match[4].get_mem() latency=2
- #pragma HLS resource variable=match[5].get_mem() latency=2
- #pragma HLS resource variable=match[6].get_mem() latency=2
- #pragma HLS resource variable=match[7].get_mem() latency=2
- #pragma HLS resource variable=allstub->get_mem() latency=2
- #pragma HLS resource variable=allproj->get_mem() latency=2
+////////////////////////////////////////////////////////////////////////////////
+// Top functions for various MatchCalculators (MC). For each iteration of
+// the main processing loop, a MC retrieves an array of Candidate Matches,
+// and computes Full Matches from each.
+////////////////////////////////////////////////////////////////////////////////
 
-  const auto LAYER  = TF::L3;
-  const auto DISK   = TF::D1;
-  const auto PHISEC = MC::C;
-  MC_L3PHIC: MatchCalculator<MC::ASRegion<LAYER>(), MC::APRegion<LAYER>(), MC::FMRegion<LAYER>(), maxMatchCopies, maxFullMatchCopies, LAYER, DISK, PHISEC>
-    (
-      bx,
-      match,
-      allstub, allproj, bx_o,
-      fullmatch
-    );
+void MatchCalculator_L3PHIC(
+    const BXType bx,
+    const CandidateMatchMemory match[maxMatchCopies],
+    const AllStubMemory<BARRELPS>* allstub,
+    const AllProjectionMemory<BARRELPS>* allproj,
+    BXType& bx_o,
+    FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
+) {
+#pragma HLS inline off
+#pragma HLS interface register port=bx_o
+#pragma HLS resource variable=match[0].get_mem() latency=2
+#pragma HLS resource variable=match[1].get_mem() latency=2
+#pragma HLS resource variable=match[2].get_mem() latency=2
+#pragma HLS resource variable=match[3].get_mem() latency=2
+#pragma HLS resource variable=match[4].get_mem() latency=2
+#pragma HLS resource variable=match[5].get_mem() latency=2
+#pragma HLS resource variable=match[6].get_mem() latency=2
+#pragma HLS resource variable=match[7].get_mem() latency=2
+#pragma HLS resource variable=allstub->get_mem() latency=2
+#pragma HLS resource variable=allproj->get_mem() latency=2
 
-} 
+MC_L3PHIC: MatchCalculator<MC::ASRegion<TF::L3>(), MC::APRegion<TF::L3>(), MC::FMRegion<TF::L3>(), 8, 8,
+ TF::L3, TF::D1, MC::C> (
+    bx,
+    match,
+    allstub,
+    allproj,
+    bx_o,
+    fullmatch
+  );
+}
+
+////////////////////////////////////////////////////////////////////////////////
