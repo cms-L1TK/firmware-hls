5c1ff70e456fe71ed8b3d135425d8932313b2990
Brent Yates <brentRyates@gmail.com>
2021-07-15 14:30:51 -0400

    MC top level and tcl from generate_top branch

--- TrackletAlgorithm/MatchCalculatorTop.h
+++ TrackletAlgorithm/MatchCalculatorTop.h
@@ -1,17 +1,54 @@
-#ifndef TrackletAlgorithm_MatchCalculatorTop_h
-#define TrackletAlgorithm_MatchCalculatorTop_h
+#ifndef TrackAlgorithm_MatchCalculatorTop_h
+#define TrackAlgorithm_MatchCalculatorTop_h
 
 #include "MatchCalculator.h"
 
 constexpr int maxMatchCopies(8);
 constexpr int maxFullMatchCopies(8);
 
-void MatchCalculatorTop(BXType bx,
-                        const CandidateMatchMemory match[maxMatchCopies],
-                        const AllStubMemory<BARRELPS>*,
-                        const AllProjectionMemory<BARRELPS>*,
-                        BXType&,
-                        FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
-                       );
+void MatchCalculator_L3PHIB(
+    const BXType bx,
+    const CandidateMatchMemory match[maxMatchCopies],
+    const AllStubMemory<BARRELPS>* allstub,
+    const AllProjectionMemory<BARRELPS>* allproj,
+    BXType& bx_o,
+    FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
+);
+
+void MatchCalculator_L3PHIC(
+    const BXType bx,
+    const CandidateMatchMemory match[maxMatchCopies],
+    const AllStubMemory<BARRELPS>* allstub,
+    const AllProjectionMemory<BARRELPS>* allproj,
+    BXType& bx_o,
+    FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
+);
+
+void MatchCalculator_L4PHIB(
+    const BXType bx,
+    const CandidateMatchMemory match[maxMatchCopies],
+    const AllStubMemory<BARREL2S>* allstub,
+    const AllProjectionMemory<BARREL2S>* allproj,
+    BXType& bx_o,
+    FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
+);
+
+void MatchCalculator_L5PHIB(
+    const BXType bx,
+    const CandidateMatchMemory match[maxMatchCopies],
+    const AllStubMemory<BARREL2S>* allstub,
+    const AllProjectionMemory<BARREL2S>* allproj,
+    BXType& bx_o,
+    FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
+);
+
+void MatchCalculator_L6PHIB(
+    const BXType bx,
+    const CandidateMatchMemory match[maxMatchCopies],
+    const AllStubMemory<BARREL2S>* allstub,
+    const AllProjectionMemory<BARREL2S>* allproj,
+    BXType& bx_o,
+    FullMatchMemory<BARREL> fullmatch[maxFullMatchCopies]
+);
 
 #endif
