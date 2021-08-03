af4625f62e1915e21f8b8bf6e7909d11192fac69
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2021-08-03 14:31:49 +0200

    Removed debug pring from IR top level generation script

--- emData/generate_IR.py
+++ emData/generate_IR.py
@@ -23,7 +23,6 @@ def getNmemsPerIR(wiresFiles='./LUTs/wires.dat') :
     for line in lines : 
       if re.search(dtcName, line, re.IGNORECASE) and ("neg" in dtcName) == ("neg" in line) : 
           irLines.append(line)
-    print(dtcName, ' : ' , len(irLines))
     nMemories.append(len(irLines))
   return zip(dtcNames, nMemories)
 
