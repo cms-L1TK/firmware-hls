222e0b979ff2f76b4fde210e90449cef2c46fe1f
kwalkingshaw <katie.walkingshawpass@bristol.ac.uk>
2021-08-03 13:38:01 +0100

    Removing checks from dummy module

--- TrackletAlgorithm/TrackMerger.cc
+++ TrackletAlgorithm/TrackMerger.cc
@@ -21,9 +21,8 @@ void TrackMerger(const BXType bx,
         TrackFit trkFit;
         trkFit.setTrackWord(trackWord[i]);
         ap_uint<TrackFitBase::kTFHitMapSize> trackStubMap = trkFit.getHitMap();
-        std::cout << "trackWord: " << trackWord[i] << " " << "Track Stub Map: " << trackStubMap<< std::endl;
+        
         //auto trackHitCount = trkFit.getHitCount();
-        //std::cout << "Track Stub Map: " << trackStubMap << std::endl;
         // every 3 bits is a new layer/disk - getting number of stubs in each layer
         //for (int layer = 0; layer < trackStubMap.length()/TrackFit::kTFHitCountSize; layer++){
              
@@ -31,19 +30,15 @@ void TrackMerger(const BXType bx,
             switch (j){
               case 0:
                 trkFit.setBarrelStubWord<0>(barrelStubWords[0][i]);
-                std::cout << "Barrel: " << trkFit.getBarrelStubWord<0>() << std::endl;
                 break;
               case 1:
                 trkFit.setBarrelStubWord<1>(barrelStubWords[1][i]);
-                std::cout << "Barrel: " << trkFit.getBarrelStubWord<1>() << std::endl;
                 break;
               case 2:
                 trkFit.setBarrelStubWord<2>(barrelStubWords[2][i]);
-                std::cout << "Barrel: " << trkFit.getBarrelStubWord<2>() << std::endl;
                 break;
               case 3:
                 trkFit.setBarrelStubWord<3>(barrelStubWords[3][i]);
-                std::cout << "Barrel: " << trkFit.getBarrelStubWord<3>() << std::endl;
                 break;
 
             }
@@ -53,59 +48,46 @@ void TrackMerger(const BXType bx,
             switch (k){
               case 4:
                 trkFit.setDiskStubWord<4>(diskStubWords[0][i]);
-                std::cout << "Disk: " << trkFit.getDiskStubWord<4>() << std::endl;
                 break;
               case 5:
                 trkFit.setDiskStubWord<5>(diskStubWords[1][i]);
-                std::cout << "Disk: " << trkFit.getDiskStubWord<5>() << std::endl;
                 break;
               case 6:
                 trkFit.setDiskStubWord<6>(diskStubWords[2][i]);
-                std::cout << "Disk: " << trkFit.getDiskStubWord<6>() << std::endl;
                 break;
               case 7:
                 trkFit.setDiskStubWord<7>(diskStubWords[3][i]);
-                std::cout << "Disk: " << trkFit.getDiskStubWord<7>() << std::endl;
                 break;
             }
         }
 
-        // getting stub index and phi residual
-        int stubIndex[TrackFit::kNStubs];
-        double phiRes[TrackFit::kNStubs];
-        for (int l = 0; l < TrackFit::kNStubs; l++){
+        // getting stub index
+        ap_uint<TrackFitBase::kTFStubIndexSize> stubIndex;
+        for (unsigned int l = 0; l < TrackFit::kNStubs; l++){
           switch (l) {
             case 0:
-              stubIndex[0] = trkFit.getStubIndex<0>();
-              phiRes[0] = trkFit.getStubPhiResid<0>();
+              stubIndex = trkFit.getStubIndex<0>();
               break;
             case 1:
-              stubIndex[1] = trkFit.getStubIndex<1>();
-              phiRes[1] = trkFit.getStubPhiResid<1>();
+              stubIndex = trkFit.getStubIndex<1>();
               break;
             case 2:
-              stubIndex[2] = trkFit.getStubIndex<2>();
-              phiRes[2] = trkFit.getStubPhiResid<2>();
+              stubIndex = trkFit.getStubIndex<2>();
               break;
             case 3: 
-              stubIndex[3] = trkFit.getStubIndex<3>();
-              phiRes[3] = trkFit.getStubPhiResid<3>();
+              stubIndex = trkFit.getStubIndex<3>();
               break;
             case 4:
-              stubIndex[4] = trkFit.getStubIndex<4>();
-              phiRes[4] = trkFit.getStubPhiResid<4>();
+              stubIndex = trkFit.getStubIndex<4>();
               break;
             case 5:
-              stubIndex[5] = trkFit.getStubIndex<5>();
-              phiRes[5] = trkFit.getStubPhiResid<5>();
+              stubIndex = trkFit.getStubIndex<5>();
               break;
             case 6:
-              stubIndex[6] = trkFit.getStubIndex<6>();
-              phiRes[6] = trkFit.getStubPhiResid<6>();
+              stubIndex = trkFit.getStubIndex<6>();
               break;
             case 7:
-              stubIndex[7] = trkFit.getStubIndex<7>();
-              phiRes[7] = trkFit.getStubPhiResid<7>();
+              stubIndex = trkFit.getStubIndex<7>();
               break;
           }
         }
