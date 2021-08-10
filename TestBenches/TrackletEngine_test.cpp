6e988bdca97842d07af866efe2230b426deacbc0
Andrew Hart <ahart@cern.ch>
2021-08-09 20:04:48 -0400

    Partially updated to use TBHelper.

--- TestBenches/TrackletEngine_test.cpp
+++ TestBenches/TrackletEngine_test.cpp
@@ -17,37 +17,23 @@ const int nevents = 100;  // number of events to run
 using namespace std;
 
 int main(){
+  TBHelper tb("TE/TE_L1PHIC12_L2PHIB12");
+
   // error counter
   int err_count = 0;
   
   // declare input memory arrays to be read from emulations files
   VMStubTEInnerMemory<BARRELPS> inputvmstubsinner;
   VMStubTEOuterMemory<BARRELPS> inputvmstubsouter;
-  //StubPairMemory inputstubpairs;
 
   // declare the output memory array for the sub pairs
   StubPairMemory outputstubpairs; //produced by hls simulation
 
-
   // open input files from emulation
-  // TE_L1PHIE18_L2PHIC17
-  //ifstream fin_vmstubsinner("../../../../../emData/TE/TE_L1PHIE18_L2PHIC17/VMStubs_VMSTE_L1PHIE18n2_04.dat");
-  //ifstream fin_vmstubsouter("../../../../../emData/TE/TE_L1PHIE18_L2PHIC17/VMStubs_VMSTE_L2PHIC17n4_04.dat");
-  //ifstream fin_stubpairs("../../../../../emData/TE/TE_L1PHIE18_L2PHIC17/StubPairs_SP_L1PHIE18_L2PHIC17_04.dat");  
-  // TE_L1PHIC12_L2PHIB12/
-  ifstream fin_vmstubsinner("../../../../../emData/TE/TE_L1PHIC12_L2PHIB12/VMStubs_VMSTE_L1PHIC12n3_04.dat");
-  ifstream fin_vmstubsouter("../../../../../emData/TE/TE_L1PHIC12_L2PHIB12/VMStubs_VMSTE_L2PHIB12n3_04.dat");
-  ifstream fin_stubpairs("../../../../../emData/TE/TE_L1PHIC12_L2PHIB12/StubPairs_SP_L1PHIC12_L2PHIB12_04.dat");  
-  assert(fin_vmstubsinner.good());
-  assert(fin_vmstubsouter.good());
-  assert(fin_stubpairs.good());
+  auto &fin_vmstubsinner = tb.files("VMStubs_VMSTE_L1*");
+  auto &fin_vmstubsouter = tb.files("VMStubs_VMSTE_L2*");
+  auto &fin_stubpairs = tb.files("StubPairs_SP_*");
 
-  // TE_L1PHIE18_L2PHIC17
-  //ap_uint<1> bendinnertable[256] =
-  //#include "../emData/TE/tables/TE_L1PHIE18_L2PHIC17_stubptinnercut.tab"
-  //ap_uint<1> bendoutertable[256] =
-  //#include "../emData/TE/tables/TE_L1PHIE18_L2PHIC17_stubptoutercut.tab"
-  // TE_L1PHIC12_L2PHIB12/
   ap_uint<1> bendinnertable[256] =
 #include "../emData/TE/tables/TE_L1PHIC12_L2PHIB12_stubptinnercut.tab"
   ap_uint<1> bendoutertable[256] =
@@ -60,8 +46,8 @@ int main(){
     outputstubpairs.clear();
 
     //read next event from the input files
-    writeMemFromFile<VMStubTEInnerMemory<BARRELPS> >(inputvmstubsinner, fin_vmstubsinner,ievt);
-    writeMemFromFile<VMStubTEOuterMemory<BARRELPS> >(inputvmstubsouter, fin_vmstubsouter,ievt);
+    writeMemFromFile<VMStubTEInnerMemory<BARRELPS> >(inputvmstubsinner, fin_vmstubsinner.at(0), ievt);
+    writeMemFromFile<VMStubTEOuterMemory<BARRELPS> >(inputvmstubsouter, fin_vmstubsouter.at(0), ievt);
 
     //set the bunch crossing
     BXType bx=ievt&0x7;
@@ -73,15 +59,10 @@ int main(){
     bool truncation = false;
 
     // compare calculated outputs with those read from emulation printout
-    err_count += compareMemWithFile<StubPairMemory>(outputstubpairs,fin_stubpairs,ievt,"StubPair",truncation);
+    err_count += compareMemWithFile<StubPairMemory>(outputstubpairs, fin_stubpairs.at(0), ievt, "StubPair", truncation);
 
   }  // end of event loop
 
-  // close files
-  fin_vmstubsinner.close();
-  fin_vmstubsouter.close();
-  fin_stubpairs.close();
-  
   // This is necessary because HLS seems to only return an 8-bit error count, so if err%256==0, the test bench can falsely pass
   if (err_count > 255) err_count = 255;
   return err_count;
