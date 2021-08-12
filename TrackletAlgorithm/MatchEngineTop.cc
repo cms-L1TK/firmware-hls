4bec70e5c267ef21bb0ecb09efd6c83275924a3a
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2021-08-12 13:24:13 -0600

    Fix one problem with running ME in Vitis HLS (remove a warning). Unfortunately, there is something else in the code that Vitis HLS doesn't like (it has an unspecific  pre-synthesis failure). Looking at the forums it appears this might be fixed or show more information in Vitis HLS 2021.1. For now, I am allowing the Vitis HLS job to fail.

--- TrackletAlgorithm/MatchEngineTop.cc
+++ TrackletAlgorithm/MatchEngineTop.cc
@@ -5,12 +5,13 @@ void MatchEngineTop(const BXType bx, BXType& bx_o,
 					const VMProjectionMemory<kProjectionType>& inputProjectionData,
 					CandidateMatchMemory& outputCandidateMatch) {
 
-#pragma HLS interface register port=bx_o
 #ifdef  __VITIS_HLS__
     // For use with Vitis >=2020.1
+    #pragma HLS LATENCY min=1 max=1
     #pragma HLS interface ap_memory port=inputStubData->get_mem() latency=2
     #pragma HLS interface ap_memory port=inputProjectionData->get_mem() latency=2
 #else
+	#pragma HLS interface register port=bx_o
     #pragma HLS resource variable=inputStubData->get_mem() latency=2
     #pragma HLS resource variable=inputProjectionData->get_mem() latency=2
 #endif
