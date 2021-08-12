c642479e049228f37ab16b711501fb16ff72fcba
Andrew Hart <ahart@cern.ch>
2021-08-12 14:24:50 -0400

    Open a new project for each top function.

--- project/script_VMR.tcl
+++ project/script_VMR.tcl
@@ -6,9 +6,6 @@
 # get some information about the executable and environment
 source env_hls.tcl
 
-# create new project (deleting any existing one of same name)
-open_project -reset vmrouter
-
 # source files
 set CFLAGS {-std=c++11 -I../TrackletAlgorithm -I../TopFunctions}
 set_top VMRouterTop_L2PHIA
@@ -18,36 +15,48 @@ add_files -tb ../TestBenches/VMRouter_test.cpp -cflags "$CFLAGS"
 # data files
 add_files -tb ../emData/VMR/
 
+open_project -reset vmrouter_L1PHID
+add_files ../TrackletAlgorithm/VMRouterTop_L1PHID.cc -cflags "$CFLAGS"
 open_solution "solution_L1PHID"
 source settings_hls.tcl
 set_top VMRouterTop_L1PHID
 csynth_design
 export_design -format ip_catalog
 
+open_project -reset vmrouter_L2PHIB
+add_files ../TrackletAlgorithm/VMRouterTop_L2PHIB.cc -cflags "$CFLAGS"
 open_solution "solution_L2PHIB"
 source settings_hls.tcl
 set_top VMRouterTop_L2PHIB
 csynth_design
 export_design -format ip_catalog
 
+open_project -reset vmrouter_L3PHIB
+add_files ../TrackletAlgorithm/VMRouterTop_L3PHIB.cc -cflags "$CFLAGS"
 open_solution "solution_L3PHIB"
 source settings_hls.tcl
 set_top VMRouterTop_L3PHIB
 csynth_design
 export_design -format ip_catalog
 
+open_project -reset vmrouter_L4PHIB
+add_files ../TrackletAlgorithm/VMRouterTop_L4PHIB.cc -cflags "$CFLAGS"
 open_solution "solution_L4PHIB"
 source settings_hls.tcl
 set_top VMRouterTop_L4PHIB
 csynth_design
 export_design -format ip_catalog
 
+open_project -reset vmrouter_L5PHIB
+add_files ../TrackletAlgorithm/VMRouterTop_L5PHIB.cc -cflags "$CFLAGS"
 open_solution "solution_L5PHIB"
 source settings_hls.tcl
 set_top VMRouterTop_L5PHIB
 csynth_design
 export_design -format ip_catalog
 
+open_project -reset vmrouter_L6PHIB
+add_files ../TrackletAlgorithm/VMRouterTop_L6PHIB.cc -cflags "$CFLAGS"
 open_solution "solution_L6PHIB"
 source settings_hls.tcl
 set_top VMRouterTop_L6PHIB
