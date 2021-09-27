3a17b09e903b9c5f1e444cb1c9f1e7b718215604
Andrew Hart <ahart@cern.ch>
2021-09-27 13:03:29 -0400

    Build HLS IPs in current directory.

---
+++ IntegrationTests/IRVMR/script/compileHLS.tcl
@@ -0,0 +1,28 @@
+# Common settings
+source ../../../project/env_hls.tcl
+set CFLAGS {-std=c++11 -I../../../TrackletAlgorithm -I../../../TopFunctions}
+set cwd [pwd]
+
+# Build IR_DTC_PS10G_3_A
+open_project -reset inputrouter
+add_files ../../../TopFunctions/InputRouterTop.cc -cflags "$CFLAGS"
+set_top InputRouterTop_IR_DTC_PS10G_3_A
+open_solution solution_PS10G_3_A
+cd ../../../project
+source settings_hls.tcl
+cd $cwd
+csynth_design
+export_design -format ip_catalog
+
+# Build VMR_L2PHIA
+open_project -reset vmrouter
+add_files ../../../TopFunctions/VMRouterTop.cc -cflags "$CFLAGS"
+set_top VMRouterTop_L2PHIA
+open_solution solution_L2PHIA
+cd ../../../project
+source settings_hls.tcl
+cd $cwd
+csynth_design
+export_design -format ip_catalog
+
+exit
