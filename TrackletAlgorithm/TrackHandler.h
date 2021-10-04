8d8109d759142dfb6193b7d7af5b57773412fb18
kwalkingshaw <katie.walkingshawpass@bristol.ac.uk>
2021-10-04 10:28:40 +0100

    Pushing updated code

---
+++ TrackletAlgorithm/TrackHandler.h
@@ -0,0 +1,64 @@
+#include "TrackFitMemory.h"
+
+const unsigned int kFullBarrelStubSize = TrackFit::kBarrelStubSize * 4;
+const unsigned int kFullDiskStubSize = TrackFit::kDiskStubSize * 4;
+const unsigned int kFullTrackWordSize = TrackFit::kTrackWordSize;
+
+class TrackHandler{
+  public:
+    TrackHandler(){};
+    TrackHandler(const TrackFit::TrackWord trackWord,
+      const TrackFit::BarrelStubWord barrelStubWords[4],
+      const TrackFit::DiskStubWord diskStubWords[4])
+    {
+      //prepare the TrackHandler members
+      //assign the bits to trackWord, barrelStubsWord, diskStubsWord
+      // set the barrel and disk stub words in constructor using loop
+      //all the information from the track and stub words;
+      trkWord = trackWord;
+      for (unsigned int j = 0; j < TrackFit::kNBarrelStubs; j++){ 
+        barrelStubArray[j] = barrelStubWords[j];
+      }
+
+    for (unsigned int k = 0; k < TrackFit::kNDiskStubs; k++){
+        diskStubArray[k] = diskStubWords[k];
+    }
+    
+    }
+  
+    ~TrackHandler(){};
+    TrackFit::TrackWord trkWord;
+    TrackFit::BarrelStubWord barrelStubArray[4];
+    TrackFit::DiskStubWord diskStubArray[4];
+    nextTrack;
+
+
+    TrackFit::TFSTUBINDEX getStubIndex(int Hit) const {
+      assert(Hit >= 0 && Hit <= kNStubs - 1);
+      return track.range(TrackFitBits::kTFStubIndexMSB(Hit),TrackFitBits::kTFStubIndexLSB(Hit));
+    }
+    // TFSTUBPHIRESID getStubPhiResid(int Hit) const {
+    //   assert(Hit >= 0 && Hit <= kNStubs - 1);
+    //   return track.range(TrackFitBits::kTFStubPhiResidMSB(Hit),TrackFitBits::kTFStubPhiResidLSB(Hit));
+    // }
+    // BarrelStubWord getBarrelStubWord(int Hit) const {
+    //   assert(Hit >= 0 && Hit <= kNBarrelStubs - 1);
+    //   return track.range(TrackFitBits::kTFStubValidMSB(Hit),TrackFitBits::kTFStubRZResidLSB(Hit));
+    // }
+    // DiskStubWord getDiskStubWord(int Hit) const {
+    //   assert(Hit >= kNBarrelStubs && Hit <= kNStubs - 1);
+    //   return track.range(TrackFitBits::kTFStubValidMSB(Hit),TrackFitBits::kTFStubRZResidLSB(Hit));
+    // }
+    // void setBarrelStubWord(const BarrelStubWord &word, int Hit) {
+    //   assert(Hit >= 0 && Hit <= kNBarrelStubs - 1);
+    //   track.range(TrackFitBits::kTFStubValidMSB(Hit),TrackFitBits::kTFStubRZResidLSB(Hit)) = word;
+    // }
+    // void setDiskStubWord(const DiskStubWord &word, int Hit) {
+    //   assert(Hit >= kNBarrelStubs && Hit <= kNStubs - 1);
+    //   track.range(TrackFitBits::kTFStubValidMSB(Hit),TrackFitBits::kTFStubRZResidLSB(Hit)) = word;
+    // }
+
+
+
+  private:
+};
