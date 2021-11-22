afe8c8f40295f52c14b6758a6c5d89a5190b3aae
Andrew Hart <ahart@cern.ch>
2021-11-22 10:57:55 -0500

    Use 100 events by default.

--- emData/download.sh
+++ emData/download.sh
@@ -4,9 +4,9 @@ set -e
 #### reduced config ####
 #### https://github.com/trholmes/cmssw/tree/7fa70ef966862a47357263173e482bc102511037 ####
 # 1 event
-memprints_url_reduced="https://cernbox.cern.ch/index.php/s/lzpcKjd1oekoYBI/download"
+#memprints_url_reduced="https://cernbox.cern.ch/index.php/s/lzpcKjd1oekoYBI/download"
 # 100 events
-#memprints_url_reduced="https://cernbox.cern.ch/index.php/s/LkVZR8WRYGPJcPe/download"
+memprints_url_reduced="https://cernbox.cern.ch/index.php/s/LkVZR8WRYGPJcPe/download"
 luts_url_reduced="https://cernbox.cern.ch/index.php/s/2zppC0iJ3eEy5C9/download"
 
 #### fw_synch_210611 ####
