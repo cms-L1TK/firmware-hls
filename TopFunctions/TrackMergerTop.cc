f10b994a727bc69706fdc5351d852be2ba48daa1
kwalkingshaw <katie.walkingshawpass@bristol.ac.uk>
2021-12-28 17:53:59 +0000

    Fixing CI error

--- TopFunctions/TrackMergerTop.cc
+++ TopFunctions/TrackMergerTop.cc
@@ -1,12 +1,12 @@
 #include "TrackMergerTop.h"
 
 void TrackMergerTop(const BXType bx,
-  const TrackFit::TrackWord trackWord [kMaxProc],
-  const TrackFit::BarrelStubWord barrelStubWords[4][kMaxProc],
-  const TrackFit::DiskStubWord diskStubWords[4][kMaxProc],
+  const TrackFit<kMaxBrlStbs,kMaxDskStbs>::TrackWord trackWord [kMaxProc],
+  const TrackFit<kMaxBrlStbs,kMaxDskStbs>::BarrelStubWord barrelStubWords[4][kMaxProc],
+  const TrackFit<kMaxBrlStbs,kMaxDskStbs>::DiskStubWord diskStubWords[4][kMaxProc],
   BXType &bx_o,
-  TrackFit::TrackWord trackWord_o [kMaxProc],
-  TrackFit::BarrelStubWord barrelStubWords_o[4][kMaxProc],
-  TrackFit::DiskStubWord diskStubWords_o[4][kMaxProc]){
+  TrackFit<kMaxBrlStbs,kMaxDskStbs>::TrackWord trackWord_o [kMaxProc],
+  TrackFit<kMaxBrlStbs,kMaxDskStbs>::BarrelStubWord barrelStubWords_o[4][kMaxProc],
+  TrackFit<kMaxBrlStbs,kMaxDskStbs>::DiskStubWord diskStubWords_o[4][kMaxProc]){
     TrackMerger(bx, trackWord, barrelStubWords, diskStubWords, bx_o, trackWord_o, barrelStubWords_o, diskStubWords_o);
 }
