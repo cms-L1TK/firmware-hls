c745a012b2a74bb36226468ee039a7e1fc4bab0d
joel-goldstein <joel.goldstein@bristol.ac.uk>
2022-07-12 09:30:09 +0100

    Working hls-stream version

--- TrackletAlgorithm/TrackMerger.cc
+++ TrackletAlgorithm/TrackMerger.cc
@@ -1,92 +1,37 @@
 #include "TrackMerger.h"
 #include <bitset>
 
-void ModuleBuffer::insertTrack(const TrackHandler track){
-  #pragma HLS inline
-  assert(writeIndex < kNBufferSize);
-  trackArray[writeIndex] = track;
-  writeIndex++;
-}
-
-TrackHandler ModuleBuffer::readTrack(){
-  #pragma HLS inline
-  assert(readIndex <= writeIndex);
-  if (readIndex < writeIndex){
-    TrackHandler track = trackArray[readIndex];
-    readIndex++;
-    return track;
-  } // else return a null track
-  else{ 
-     return TrackHandler();
-  }
-}
-
-void ModuleBuffer::clearBuffer(){
-  #pragma HLS inline
-  TrackHandler track;
-  readIndex = 0;
-  writeIndex = 0;
-
-}
-
-void ComparisonModule::process(hls::stream<TrackHandler> &inputBuffer, hls::stream<TrackHandler> &outputBuffer){
+void ComparisonModule::process(hls::stream<track_struct> &inputBuffer, hls::stream<track_struct> &outputBuffer){
   #pragma HLS inline
   #pragma HLS pipeline
-  TrackHandler track;
+  track_struct track;
   if (inputBuffer.read_nb(track)) {
-    assert(track.getTrackWord()!=0 && "Null track in CM");
+    assert(track._trackWord !=0 && "Null track in CM");
     outputBuffer.write(track);
     tracksProcessed++;
   }
 }
 
-void ComparisonModule::process(hls::stream<TrackFit::TrackWord> &inputBuffer, hls::stream<TrackFit::TrackWord> &outputBuffer){
-  #pragma HLS inline
-  #pragma HLS pipeline
-  TrackFit::TrackWord track;
-  if (inputBuffer.read_nb(track)) {
-    assert(track !=0 && "Null track in CM");
-    outputBuffer.write(track);
-    tracksProcessed++;
-  }
-}
-
-void ComparisonModule::process(ModuleBuffer &inputBuffer, ModuleBuffer &outputBuffer){
-  #pragma HLS inline
-  #pragma HLS pipeline
-  TrackHandler track = inputBuffer.readTrack();
-
-  if (track.getTrackWord() != 0){    
-    outputBuffer.insertTrack(track);
-    tracksProcessed++;
-  }
-}
-
-ModuleBuffer::ModuleBuffer() : trackArray() {}
-
-ModuleBuffer::~ModuleBuffer(){}
-
-// overall module only reads in a track and pass it to the comparison module
 void TrackMerger(const BXType bx,
   hls::stream<TrackFit::TrackWord> &trackWord,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_0,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_1,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_2,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_3,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_0,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_1,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_2,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_3,
   BXType bx_o,
-  hls::stream<TrackFit::TrackWord> &trackWord_o
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0_o, 
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1_o, 
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2_o, 
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_0_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_1_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_2_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_3_o 
+  hls::stream<TrackFit::TrackWord> &trackWord_o,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0_o, 
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1_o, 
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2_o, 
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_0_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_1_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_2_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_3_o 
   )
 {
     #pragma HLS inline
@@ -94,36 +39,23 @@ void TrackMerger(const BXType bx,
     ComparisonModule comparisonModule[kNComparisonModules];
     #pragma HLS array_partition variable=comparisonModule complete dim=0
 
-    // ModuleBuffer buffer[kNBuffers];
-    // #pragma HLS array_partition variable=buffer complete dim=0
-    hls::stream<TrackFit::TrackWord> buffer[kNBuffers];
-    #pragma HLS STREAM variable=buffer depth=3
-
-    unsigned int nIn = 0;
-    unsigned int nOut = 0;
-
-    // assert(buffer[0].empty());
-    // assert(buffer[1].empty());
+    hls::stream<track_struct> buffer[kNBuffers];
+    #pragma HLS STREAM variable=buffer depth=2
 
     LOOP_PROCESS:
     for (int i = 0; i < kMaxProc; i++){ 
       #pragma HLS pipeline II=1 REWIND
-      TrackFit::TrackWord inTrackWord;
-      TrackFit::BarrelStubWord barrelStubWordsArray[4];
-      TrackFit::DiskStubWord diskStubWordsArray[4];
-      if (trackWord.read_nb(inTrackWord)) {
-        // assert(barrelStubWords_0.read_nb(barrelStubWordsArray[0]));
-        // assert(barrelStubWords_1.read_nb(barrelStubWordsArray[1]));
-        // assert(barrelStubWords_2.read_nb(barrelStubWordsArray[2]));
-        // assert(barrelStubWords_3.read_nb(barrelStubWordsArray[3]));
-        // assert(diskStubWords_0.read_nb(diskStubWordsArray[0]));
-        // assert(diskStubWords_1.read_nb(diskStubWordsArray[1]));
-        // assert(diskStubWords_2.read_nb(diskStubWordsArray[2]));
-        // assert(diskStubWords_3.read_nb(diskStubWordsArray[3]));
-        //TrackHandler track(inTrackWord, barrelStubWordsArray, diskStubWordsArray);
-        //buffer[0].insertTrack(track);
-        buffer[0].write(inTrackWord);
-        ++nIn;
+      track_struct theTrack;
+      if (trackWord.read_nb(theTrack._trackWord)) {
+        assert(barrelStubWords_0.read_nb(theTrack._barrelStub_0));
+        assert(barrelStubWords_1.read_nb(theTrack._barrelStub_1));
+        assert(barrelStubWords_2.read_nb(theTrack._barrelStub_2));
+        assert(barrelStubWords_3.read_nb(theTrack._barrelStub_3));
+        assert(diskStubWords_0.read_nb(theTrack._diskStub_0));        
+        assert(diskStubWords_1.read_nb(theTrack._diskStub_1));        
+        assert(diskStubWords_2.read_nb(theTrack._diskStub_2));        
+        assert(diskStubWords_3.read_nb(theTrack._diskStub_3));        
+        buffer[0].write(theTrack);
       }
 
       LOOP_ProcTracks:
@@ -132,23 +64,21 @@ void TrackMerger(const BXType bx,
         comparisonModule[j].process(buffer[j], buffer[j+1]);
       } 
 
-      TrackFit::TrackWord track_o; 
+      track_struct track_o; 
       if (!buffer[kNBuffers-1].empty()) {
-        if (buffer[kNBuffers-1].read_nb(track_o) ) {
-          assert(track_o != 0 && "Null track in FIFO"); // Check it is not a null track
-          trackWord_o.write(track_o);
-          // barrelStubWords_0_o.write(track_o.getBarrelStubArray(0,0));
-          // barrelStubWords_1_o.write(track_o.getBarrelStubArray(1,0));
-          // barrelStubWords_2_o.write(track_o.getBarrelStubArray(2,0));
-          // barrelStubWords_3_o.write(track_o.getBarrelStubArray(3,0));
-          // diskStubWords_0_o.write(track_o.getDiskStubArray(0,0));
-          // diskStubWords_1_o.write(track_o.getDiskStubArray(1,0));
-          // diskStubWords_2_o.write(track_o.getDiskStubArray(2,0));
-          // diskStubWords_3_o.write(track_o.getDiskStubArray(3,0));
-          ++nOut;
+        if (buffer[kNBuffers-1].read_nb(track_o)) {
+          assert(track_o._trackWord != 0 && "Null track in FIFO"); // Check it is not a null track
+          trackWord_o.write(track_o._trackWord);
+          barrelStubWords_0_o.write(track_o._barrelStub_0);
+          barrelStubWords_1_o.write(track_o._barrelStub_1);
+          barrelStubWords_2_o.write(track_o._barrelStub_2);
+          barrelStubWords_3_o.write(track_o._barrelStub_3);
+          diskStubWords_0_o.write(track_o._diskStub_0);
+          diskStubWords_1_o.write(track_o._diskStub_1);
+          diskStubWords_2_o.write(track_o._diskStub_2);
+          diskStubWords_3_o.write(track_o._diskStub_3);
         }
       } 
     }
-    bx_o = bx;
-  std::cout << "Finishing TrackMerger with " << nIn << " tracks read and " << nOut << " out" <<std::endl;
+  bx_o = bx;
 }
--- TrackletAlgorithm/TrackMerger.h
+++ TrackletAlgorithm/TrackMerger.h
@@ -3,45 +3,32 @@
 
 #include "hls_stream.h"
 #include <cassert>
-#include "TrackHandler.h"
+#include "TrackFitMemory.h"
 
-
-const unsigned int kNComparisonModules = 10;
+const unsigned int kNComparisonModules = 16;
 const unsigned int kNBuffers = kNComparisonModules + 1;
-const unsigned int kNTracks = 50;
-const unsigned int kNBufferSize = kNTracks;
 
-class ModuleBuffer{
-  public:
-    ModuleBuffer();
-
-    ~ModuleBuffer();
-    
-    void insertTrack(const TrackHandler track);
-
-    TrackHandler readTrack();
-
-    void clearBuffer();
-    
-    private:
-      unsigned int readIndex{0};
-      unsigned int writeIndex{0};
-      TrackHandler trackArray[kNBufferSize];
-};
+typedef struct {
+  TrackFit::TrackWord _trackWord;
+  TrackFit::BarrelStubWord _barrelStub_0;  
+  TrackFit::BarrelStubWord _barrelStub_1;  
+  TrackFit::BarrelStubWord _barrelStub_2;  
+  TrackFit::BarrelStubWord _barrelStub_3;  
+  TrackFit::DiskStubWord _diskStub_0;
+  TrackFit::DiskStubWord _diskStub_1;
+  TrackFit::DiskStubWord _diskStub_2;
+  TrackFit::DiskStubWord _diskStub_3;
+} track_struct;
 
 class ComparisonModule{
     public:
     ComparisonModule()
     {
       tracksProcessed = 0;
-      TrackFit::TrackWord nullWord(0);
-      masterTrack.setTrackWord(nullWord);
-      track.setTrackWord(nullWord);
-
+      masterTrack._trackWord = 0;
     }
     ~ComparisonModule(){};
 
-    
     unsigned int getMatchFound(){return matchFound;}
 
     unsigned int getNProcessed(){return tracksProcessed;}
@@ -52,24 +39,17 @@ class ComparisonModule{
 
     unsigned int getEndOfStream(){return endOfStream;}
 
-    TrackFit::TrackWord getMasterTrackWord(){return masterTrack.getTrackWord();}
+    TrackFit::TrackWord getMasterTrackWord(){return masterTrack._trackWord;}
 
-    TrackFit::BarrelStubWord getMasterTrackBarrelStubs(unsigned int stubIndex, unsigned int layerIndex){
-      return masterTrack.getBarrelStubArray(layerIndex, stubIndex);}
+    // TrackFit::BarrelStubWord getMasterTrackBarrelStubs(unsigned int stubIndex, unsigned int layerIndex){
+    //   return masterTrack.getBarrelStubArray(layerIndex, stubIndex);}
 
-    TrackFit::DiskStubWord getMasterTrackDiskStubs(unsigned int stubIndex, unsigned int layerIndex){
-      return masterTrack.getDiskStubArray(layerIndex, stubIndex);}
+    // TrackFit::DiskStubWord getMasterTrackDiskStubs(unsigned int stubIndex, unsigned int layerIndex){
+    //   return masterTrack.getDiskStubArray(layerIndex, stubIndex);}
 
     unsigned int getEndOfModule(){return endOfModule;}
 
-    void process(ModuleBuffer &inputBuffer, ModuleBuffer &outputBuffer);
-
-    void process(hls::stream<TrackHandler>& inputBuffer,hls::stream<TrackHandler>& outputBuffer);
-    void process(hls::stream<TrackFit::TrackWord>& inputBuffer,hls::stream<TrackFit::TrackWord>& outputBuffer);
-
-    // void setInputBuffer(ModuleBuffer &buffer);
-
-    // void setOutputBuffer(ModuleBuffer &buffer);
+    void process(hls::stream<track_struct>& inputBuffer,hls::stream<track_struct>& outputBuffer);
     
   private:
 
@@ -79,33 +59,30 @@ class ComparisonModule{
     unsigned int endOfStream{0};
     unsigned int endOfModule{0};
 
-    TrackHandler masterTrack;
-    TrackHandler track;
-    // ModuleBuffer inputBuffer;
-    // ModuleBuffer outputBuffer;
+    track_struct masterTrack;
 
 };
 
 void TrackMerger(const BXType bx,
   hls::stream<TrackFit::TrackWord> &trackWord,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2,
-  // const hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_0,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_1,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_2,
-  // const hls::stream<TrackFit::DiskStubWord> &diskStubWords_3,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_0,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_1,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_2,
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_3,
   BXType bx_o,
-  hls::stream<TrackFit::TrackWord> &trackWord_o
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0_o, 
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1_o, 
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2_o, 
-  // hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_0_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_1_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_2_o, 
-  // hls::stream<TrackFit::DiskStubWord> &diskStubWords_3_o 
+  hls::stream<TrackFit::TrackWord> &trackWord_o,
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_0_o, 
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_1_o, 
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_2_o, 
+  hls::stream<TrackFit::BarrelStubWord> &barrelStubWords_3_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_0_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_1_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_2_o, 
+  hls::stream<TrackFit::DiskStubWord> &diskStubWords_3_o 
   );
 
 
