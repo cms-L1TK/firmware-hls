7fdd605de88fb78c8d85d955d27d352cfcbbb42c
Brent Yates <brentRyates@gmail.com>
2022-07-27 12:04:00 -0400

    MP working in barrel layers again

--- emData/download.sh
+++ emData/download.sh
@@ -83,7 +83,7 @@ then
   mv LUTs LUTsCMReduced
   rm -f LUTs.tgz
   wget -O LUTs.tgz --quiet ${luts_url_cm}
-  tar -xzf LUTs.tgz
+  tar -xzf LUTs.tar.gz
   mv LUTs LUTsCM
   rm -f LUTs.tgz
   wget -O LUTs.tar.gz --quiet ${luts_url}
