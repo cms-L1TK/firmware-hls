c6364146cb2a851974d7478b8a15378e79c1dff7
KathrynColdham <kathryn.coldham@cern.ch>
2023-01-04 20:17:02 +0000

    pointer to pointer bug fix

--- TrackletAlgorithm/MatchEngine.h
+++ TrackletAlgorithm/MatchEngine.h
@@ -56,94 +56,80 @@ bool getIsPSModule(const typename VMProjection<ProjectionType>::VMPZBIN & rzbin,
 template<TF::layerDisk LayerDisk> inline const ap_uint<1>* readTable() {
 	printf("The LayerDisk value must be between TF::L1 (0) and TF::D5 (10)");
 	static ap_uint<1> lut[] = {};
-	printf("lut = ", lut);
+	//ap_uint<1> lut[] = {};
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::L1>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_L1.tab"
+
+	//for(ap_uint<1> i = 0; i < 512; i++){ printf("%i ", lut[i]); }
+
 	return lut;
 }
+
 template<> inline const ap_uint<1>* readTable<TF::L2>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_L2.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::L3>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_L3.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::L4>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_L4.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::L5>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_L5.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::L6>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_L6.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::D1>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_D1.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::D2>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_D2.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::D3>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_D3.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::D4>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_D4.tab"
 	return lut;
 }
 template<> inline const ap_uint<1>* readTable<TF::D5>() {
 	static ap_uint<1> lut[] = 
+	//ap_uint<1> lut[] =
 #include "../emData/ME/tables/METable_D5.tab"
 	return lut;
 }
 
-/*
-//To get the number of elements in each table
-//template<TF::layerDisk LayerDisk> inline const int nElementsInTable() {
-template<TF::layerDisk LayerDisk> inline const ap_uint<1>* nElementsInTable() {
-        printf("The LayerDisk value must be between TF::L1 (0) and TF::D5 (10)");
-        static ap_uint<1> lut[] = {};
-
-	printf("lut = ", lut);
-	//int lut[] = {};
-        return lut;
-}
-
-
-//template<> inline const int nElementsInTable<TF::D1>() {
-template<> inline const ap_uint<1>* nElementsInTable<TF::D1>(){
- 
-	static ap_uint<1> lut[] = 
-#include "../emData/ME/tables/METable_D1.tab"
-
-	//printf("lut = ", lut);
-
-	auto lutArrayLength = sizeof(lut) / sizeof(int);
-
-	printf("lutArrayLength = ", lutArrayLength);
-
-	return lutArrayLength;
-
-}
-*/
 
 
 template<TF::layerDisk LayerDisk>
@@ -203,7 +189,10 @@ void MatchEngine(const BXType bx, BXType& bx_o,
 	//
 	//Initialize table for bend-rinv consistency
 	//
-	static const ap_uint<1>* table = readTable<LayerDisk>();
+
+	//static const ap_uint<1>* table = readTable<LayerDisk>();
+	const ap_uint<1>* table = readTable<LayerDisk>();
+
 	//auto NumTableElements = nElementsInTable<LayerDisk>();
 
 	//std::cout << "NumTableElements = " << NumTableElements << std::endl;
