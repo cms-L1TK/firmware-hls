5eb734079609789b1e6b181bbbf684b7fbcb397a
Andrew Hart <ahart@cern.ch>
2023-01-05 09:12:11 -0500

    Update to VU13P (#252)
    * Moved FPGA to a single file and updated to VU13P.
    
    * Added missing file.
    
    * Moved fpga.tcl and made speed grade -2 the default.

---
+++ project/fpga.tcl
@@ -0,0 +1,7 @@
+# This file sets the FPGA part number, and is the only place this number should
+# be set. It is used by both HLS and Vivado.
+
+set FPGA xcvu13p-flga2577-2-e
+
+# Uncomment the following line if you want to test speed grade -1.
+#set FPGA xcvu13p-flga2577-1-e
