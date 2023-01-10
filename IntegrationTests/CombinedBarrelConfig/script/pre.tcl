e6d7696a1fee7531d52a4e8a7ea68b0dc7136a62
Andrew Hart <ahart@cern.ch>
2023-01-10 08:33:50 -0500

    Added pre-simulation/-synthesis file to copy LUTs.

---
+++ IntegrationTests/CombinedBarrelConfig/script/pre.tcl
@@ -0,0 +1,10 @@
+# Copy LUTs to xsim or synth_1 directory
+set cwd [file tail [pwd]]
+file delete -force ./LUTs
+if {$cwd == "xsim"} {
+  # xsim
+  file copy -force ../../../../../../../../emData/LUTsCMBarrel ./LUTs
+} else {
+  # synth_1
+  file copy -force ../../../../../../emData/LUTsCMBarrel ./LUTs
+}
