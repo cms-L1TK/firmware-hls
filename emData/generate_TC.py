0e2810d015707fdcad0967de8b3096ea7bc167a0
Peter Wittich <wittich@cornell.edu>
2023-02-01 10:07:38 -0500

    Update generate scripts for their Vitis versions for some of the updated pragmas

--- emData/generate_TC.py
+++ emData/generate_TC.py
@@ -269,13 +269,13 @@ with open(os.path.join(dirname, arguments.outputDirectory, "TrackletCalculator_p
             "#pragma HLS array_partition variable=stubPairs complete dim=1\n"
         )
         for i in range(0, nASMemInner):
-            topFile.write("#pragma HLS resource variable=innerStubs[" + str(i) + "].get_mem() latency=2\n")
+            topFile.write("#pragma HLS interface mode=ap_memory port=innerStubs[" + str(i) + "].get_mem() latency=2 storage_type=RAM_S2P\n")
         for i in range(0, nASMemOuter):
-            topFile.write("#pragma HLS resource variable=outerStubs[" + str(i) + "].get_mem() latency=2\n")
+            topFile.write("#pragma HLS interface mode=ap_memory port=outerStubs[" + str(i) + "].get_mem() latency=2\n storage_type=RAM_S2P")
         for i in range(0, nSPMem):
-            topFile.write("#pragma HLS resource variable=stubPairs[" + str(i) + "].get_mem() latency=2\n")
+            topFile.write("#pragma HLS interface mode=ap_memory port=stubPairs[" + str(i) + "].get_mem() latency=2\n storage_type=RAM_S2P")
         topFile.write(
-            "#pragma HLS interface register port=bx_o\n"
+            "#pragma HLS interface register mode=ap_vld port=bx_o\n"
             "#pragma HLS array_partition variable=projout_barrel_ps complete dim=1\n"
             "#pragma HLS array_partition variable=projout_barrel_2s complete dim=1\n"
             "#pragma HLS array_partition variable=projout_disk complete dim=1\n"
