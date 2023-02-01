74c0c715169e541640e311a670e9d41f2dc3fc17
Peter Wittich <wittich@cornell.edu>
2023-02-01 10:07:38 -0500

    Update generate scripts for their Vitis versions for some of the updated pragmas

--- emData/generate_PR.py
+++ emData/generate_PR.py
@@ -74,7 +74,7 @@ def writeSourceModuleInstance(module, wiresFileName):
     strSource += "#pragma HLS inline off\n"
     strSource += "#pragma HLS interface register port=bx_o\n"
     for i in range(TProjCount):
-        strSource += "#pragma HLS resource variable=projin[" + str(i) + "]->get_mem() latency=2\n"
+        strSource += "#pragma HLS resource variable=projin[" + str(i) + "].get_mem() latency=2\n"
     strSource += "  constexpr unsigned int nInMem = " + str(TProjCount) + ";\n"
     strSource += "  constexpr unsigned int nOutMem = " + str(VMProjCount) + ";\n"
     strSource += "  constexpr int layer = " + str(layer) + ";\n"
