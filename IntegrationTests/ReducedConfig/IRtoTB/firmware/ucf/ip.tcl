4980d4febe03336f1ec648a6592dac8da7cb3714
FilMarini <marinifil@gmail.com>
2023-02-03 10:25:48 +0100

    fixed ip ucf file for new location

--- IntegrationTests/ReducedConfig/IRtoTB/firmware/ucf/ip.tcl
+++ IntegrationTests/ReducedConfig/IRtoTB/firmware/ucf/ip.tcl
@@ -1,2 +1,2 @@
-set_property ip_repo_paths  ../../../src/firmware-hls/firmware/cgn/ [current_project]
+set_property ip_repo_paths  ../../../src/firmware-hls/IntegrationTests/ReducedConfig/IRtoTB/firmware/cgn/ [current_project]
 update_ip_catalog
