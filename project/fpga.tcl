05ce22ca9dfe88311f4b1cbdffb7e1e777591eb7
FilMarini <marinifil@gmail.com>
2023-02-03 13:51:15 +0100

    changes not needed

--- project/fpga.tcl
+++ project/fpga.tcl
@@ -1,10 +1,7 @@
 # This file sets the FPGA part number, and is the only place this number should
 # be set. It is used by both HLS and Vivado.
 
-#set FPGA xcvu13p-flga2577-2-e
+set FPGA xcvu13p-flga2577-2-e
 
 # Uncomment the following line if you want to test speed grade -1.
 #set FPGA xcvu13p-flga2577-1-e
-
-# Uncomment the following line if you want to test with VU7P
-set FPGA xcvu7p-flvb2104-2-e
