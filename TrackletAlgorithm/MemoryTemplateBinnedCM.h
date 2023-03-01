68a9698d6c5445f79189ed027f88c71ddfdb9531
Michael Carrigan <carriganm95@gmail.com>
2023-03-01 17:10:34 -0500

    updates to make disk VMStubME memories work in CMSSW

--- TrackletAlgorithm/MemoryTemplateBinnedCM.h
+++ TrackletAlgorithm/MemoryTemplateBinnedCM.h
@@ -165,6 +165,9 @@ class MemoryTemplateBinnedCM{
       nentries8A_[ibx*kNBinsRZ+ibin].range(ireg*4+3,ireg*4)=nentry_ibx+1;
       nentries8B_[ibx*kNBinsRZ+ibin].range(ireg*4+3,ireg*4)=nentry_ibx+1;
       binmask8_[ibx][ibin].set_bit(ireg,true);
+      std::cout << "ibin: " << ibin << ", ireg: " << ireg << ", slot: " << slot << ", nentries8_: " << nentries8_[ibx][ibin] << ",\nnentries8_[ibx][ibin].range(ireg*4+3,ireg*4): " <<
+		nentries8_[ibx][ibin].range(ireg*4+3,ireg*4) << ", nentry_ibx+1: " << nentry_ibx+1 << ", binmask8_: " << binmask8_[ibx][ibin] << 
+                "size of array: " << sizeof(nentries8_) / sizeof(nentries8_[0]) << " by " << sizeof(nentries8_[0]) / sizeof(ap_uint<32>) << std::endl;
       #endif
 
       return true;
@@ -234,14 +237,16 @@ class MemoryTemplateBinnedCM{
 
     int slot = (int)strtol(split(line, ' ').front().c_str(), nullptr, base); // Convert string (in hexadecimal) to int
 
+    std::cout << "*************In memory template binned **************\nknBitsRZBinCM: " << kNBitsRZBinCM << ", kNbitsphibin: " << kNbitsphibin << std::endl;   
+
     ap_uint<kNBitsRZBinCM> ibin;
     ap_uint<kNBitsphibinCM> ireg;
     (ireg,ibin)=slot;
     ap_uint<4> nentry_ibx = nentries8_[ibx][ibin].range(ireg*4+3,ireg*4);
-
     DataType data(datastr.c_str(), base);
-
+    std::cout << "pre second write mem" <<  std::endl;
     bool success = write_mem(ibx, slot, data, nentry_ibx);
+    std::cout << "write mem success: " << success << std::endl;
     #ifndef CMSSW_GIT_HASH
     if (success) {
       nentries8_[ibx][ibin].range(ireg*4+3,ireg*4)=nentry_ibx+1;
@@ -250,7 +255,7 @@ class MemoryTemplateBinnedCM{
       binmask8_[ibx][ibin].set_bit(ireg,true);
     }
     #endif
-
+    std::cout << "returning..." << std::endl;
     return success;
   }
 
