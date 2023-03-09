fd5edaa0d71974307d07d802e3b393b4fd13ce6c
Michael Carrigan <carriganm95@gmail.com>
2023-03-09 16:29:15 -0500

    remove CMSSW_GITHASH for NBIT_BX definitions

--- TrackletAlgorithm/MemoryTemplateBinnedCM.h
+++ TrackletAlgorithm/MemoryTemplateBinnedCM.h
@@ -165,9 +165,6 @@ class MemoryTemplateBinnedCM{
       nentries8A_[ibx*kNBinsRZ+ibin].range(ireg*4+3,ireg*4)=nentry_ibx+1;
       nentries8B_[ibx*kNBinsRZ+ibin].range(ireg*4+3,ireg*4)=nentry_ibx+1;
       binmask8_[ibx][ibin].set_bit(ireg,true);
-      std::cout << "ibin: " << ibin << ", ireg: " << ireg << ", slot: " << slot << ", nentries8_: " << nentries8_[ibx][ibin] << ",\nnentries8_[ibx][ibin].range(ireg*4+3,ireg*4): " <<
-		nentries8_[ibx][ibin].range(ireg*4+3,ireg*4) << ", nentry_ibx+1: " << nentry_ibx+1 << ", binmask8_: " << binmask8_[ibx][ibin] << 
-                "size of array: " << sizeof(nentries8_) / sizeof(nentries8_[0]) << " by " << sizeof(nentries8_[0]) / sizeof(ap_uint<32>) << std::endl;
       #endif
 
       return true;
@@ -237,16 +234,13 @@ class MemoryTemplateBinnedCM{
 
     int slot = (int)strtol(split(line, ' ').front().c_str(), nullptr, base); // Convert string (in hexadecimal) to int
 
-    std::cout << "*************In memory template binned **************\nknBitsRZBinCM: " << kNBitsRZBinCM << ", kNbitsphibin: " << kNbitsphibin << std::endl;   
-
     ap_uint<kNBitsRZBinCM> ibin;
     ap_uint<kNBitsphibinCM> ireg;
     (ireg,ibin)=slot;
     ap_uint<4> nentry_ibx = nentries8_[ibx][ibin].range(ireg*4+3,ireg*4);
+   
     DataType data(datastr.c_str(), base);
-    std::cout << "pre second write mem" <<  std::endl;
     bool success = write_mem(ibx, slot, data, nentry_ibx);
-    std::cout << "write mem success: " << success << std::endl;
     #ifndef CMSSW_GIT_HASH
     if (success) {
       nentries8_[ibx][ibin].range(ireg*4+3,ireg*4)=nentry_ibx+1;
@@ -255,7 +249,6 @@ class MemoryTemplateBinnedCM{
       binmask8_[ibx][ibin].set_bit(ireg,true);
     }
     #endif
-    std::cout << "returning..." << std::endl;
     return success;
   }
 
@@ -276,7 +269,7 @@ class MemoryTemplateBinnedCM{
   {
 	for(unsigned int slot=0;slot<8;slot++) {
       //std::cout << "slot "<<slot<<" entries "
-      //		<<nentries_[bx%NBX].range((slot+1)*4-1,slot*4)<<endl;
+      //                <<nentries_[bx%NBX].range((slot+1)*4-1,slot*4)<<endl;
       for (unsigned int i = 0; i < nentries8_[bx][slot]; ++i) {
 	edm::LogVerbatim("L1trackHLS") << bx << " " << i << " ";
 	print_entry(bx, i + slot*getNEntryPerBin() );
