a42a6417e0d179085b1969d8aedae12ea1946333
Andrew Hart <ahart@cern.ch>
2023-04-18 23:36:32 -0400

    Updated strategies to the best found for 2022.2.

--- IntegrationTests/common/script/impl.tcl
+++ IntegrationTests/common/script/impl.tcl
@@ -13,15 +13,12 @@ open_project ./${_xil_proj_name_}/${_xil_proj_name_}.xpr
 reset_run impl_1
 
 # Best strategy to meet timing at high frequency (but slows compilation).
-#set_property "strategy" "Performance_ExtraTimingOpt" [get_runs impl_1]
+set_property "strategy" "Performance_ExplorePostRoutePhysOpt" [get_runs impl_1]
 #set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveFanoutOpt [get_runs impl_1]
 #set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
 #set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
-
-set_property strategy Performance_ExtraTimingOpt [get_runs impl_1]
-set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraTimingOpt [get_runs impl_1]
-set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE MoreGlobalIterations [get_runs impl_1]
-set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+#set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraTimingOpt [get_runs impl_1]
+#set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE MoreGlobalIterations [get_runs impl_1]
 
 # Implementation
 update_compile_order -fileset sources_1
@@ -30,7 +27,7 @@ wait_on_run impl_1
 
 # Make reports
 open_run impl_1
-report_timing_summary -max_paths 2500 -nworst 2500 -file timing_impl.rpt
+report_timing_summary -max_paths 9999 -nworst 9999 -file timing_impl.rpt
 report_utilization -hierarchical -file utilization_impl.rpt
 
 exit
--- IntegrationTests/common/script/synth.tcl
+++ IntegrationTests/common/script/synth.tcl
@@ -13,14 +13,12 @@ open_project ./${_xil_proj_name_}/${_xil_proj_name_}.xpr
 reset_run synth_1
 
 # Best strategy to meet timing at high frequency (but slows compilation).
-#set_property "strategy" "Flow_AlternateRoutability" [get_runs synth_1]
+set_property "strategy" "Flow_PerfOptimized_high" [get_runs synth_1]
 #set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
 #set_property STEPS.SYNTH_DESIGN.ARGS.NO_LC false [get_runs synth_1]
 #set_property STEPS.SYNTH_DESIGN.ARGS.SHREG_MIN_SIZE 3 [get_runs synth_1]
 
 # Needed if design doesn't connect to I/O pins of FPGA.
-set_property strategy Flow_AlternateRoutability [get_runs synth_1]
-set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
 set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
 
 # Run pre.tcl if it exists
