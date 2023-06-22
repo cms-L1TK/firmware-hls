4746a7bb357a095a947a4cdbb14b1bb727e30a39
Andrew Hart <ahart@cern.ch>
2023-06-22 19:25:28 -0400

    Added compiler flags from CMSSW.

--- project/env_hls.tcl
+++ project/env_hls.tcl
@@ -13,3 +13,7 @@ if { ([string first "vitis" $exe] > -1) && ($year > 2019) } {
 	interp alias {} cosim_design {} interp invokehidden {} -- cosim_design -verbose
 }
 
+# CXXFLAGS adapted from CMSSW_12_6_0_pre5
+# Retrieved by running the following:
+#   $ scram b echo_CXXFLAGS
+set ::env(CCFLAG) "-Werror=main -Werror=pointer-arith -Werror=overlength-strings -Wno-vla -Werror=overflow -Werror=array-bounds -Werror=format-contains-nul -Werror=type-limits -Wall -Wno-non-template-friend -Wno-long-long -Wreturn-type -Wno-cast-function-type -Wno-unused-but-set-parameter -Wno-ignored-qualifiers -Wno-deprecated-copy -Wno-unused-parameter -Wunused -Wparentheses -Wno-deprecated -Werror=return-type -Werror=missing-braces -Werror=unused-value -Werror=address -Werror=format -Werror=sign-compare -Werror=write-strings -Werror=delete-non-virtual-dtor -Werror=strict-aliasing -Werror=narrowing -Werror=unused-but-set-variable -Werror=reorder -Werror=unused-variable -Werror=conversion-null -Werror=return-local-addr -Wnon-virtual-dtor -Werror=switch -Wno-unused-local-typedefs -Wno-attributes -Wno-psabi -Wno-unused-label"
