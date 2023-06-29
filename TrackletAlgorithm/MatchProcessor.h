2d990f464e2897ff9903aae4add05f55c884fb74
Brent Yates <brentRyates@gmail.com>
2023-06-29 12:51:12 -0400

    Add back `PhiProjBin`

--- TrackletAlgorithm/MatchProcessor.h
+++ TrackletAlgorithm/MatchProcessor.h
@@ -1526,6 +1526,7 @@ void MatchProcessor(BXType bx,
 
       unsigned int ivmPlus = iphi;
 
+      typename ProjectionRouterBuffer<BARREL, APTYPE>::PHIPROJBIN phiProjBin  = 0;
       ap_int<2> shift = 0;
       
       if (extrabits == ((1U << nextrabits) - 1) && iphi != ((1U << nbits_vmme) - 1)) {
@@ -1534,6 +1535,7 @@ void MatchProcessor(BXType bx,
       }
       unsigned int ivmMinus = iphi;
       if (extrabits == 0 && iphi != 0) {
+        phiProjBin = 1;
         shift = -1;
         ivmMinus--;
       }
@@ -1572,7 +1574,7 @@ void MatchProcessor(BXType bx,
       ap_uint<1> usesecondPlus = ivmPlus != ivmMinus && useSecond && (nstublastPlus != 0);
 
       ap_uint<4> mask = (usesecondPlus, usefirstPlus, usesecondMinus, usefirstMinus);
-      ProjectionRouterBuffer<VMPTYPE, APTYPE> projbuffertmp(allproj.raw(), ivmMinus, ivmPlus, shift, trackletid, mask, nstubs, zfirst, vmproj, psseed, usefirstMinus, usesecondMinus, usefirstPlus, usesecondPlus);
+      ProjectionRouterBuffer<VMPTYPE, APTYPE> projbuffertmp(allproj.raw(), ivmMinus, ivmPlus, phiProjBin, shift, trackletid, mask, nstubs, zfirst, vmproj, psseed, usefirstMinus, usesecondMinus, usefirstPlus, usesecondPlus);
       projbufferarray.saveProjection(projbuffertmp);
 
       if (usefirstPlus || usesecondPlus || usefirstMinus || usesecondMinus) {
