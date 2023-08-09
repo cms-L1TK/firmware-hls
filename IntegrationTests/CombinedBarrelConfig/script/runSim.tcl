5d5a649ae9fdc3456210ceaf932873043772c237
Anders Ryd <ar322@cornell.edu>
2023-08-09 17:21:35 -0400

    Minor cleanup to address comments on PR 281

--- IntegrationTests/CombinedBarrelConfig/script/runSim.tcl
+++ IntegrationTests/CombinedBarrelConfig/script/runSim.tcl
@@ -15,11 +15,7 @@ set_property -name {xsim.compile.tcl.pre} -value [get_files pre.tcl -of [get_fil
 launch_simulation
 
 # Set default wave viewer cfg
-#open_wave_config {../tb/prmemc.wcfg}
 open_wave_config {../tb/start_bx.wcfg}
-#open_wave_config {../tb/tf_top.wcfg}
-#open_wave_config {../tb/mem_tf.wcfg}
-#open_wave_config {../tb/mem_tf_wea.wcfg}
 
 restart
 # Need 4us + 0.45us per event (50us for 100 events, but 10us for quick test).
