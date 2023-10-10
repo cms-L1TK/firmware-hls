6b78beebe0d88c9710d7d88e4986a84a67a1d4a3
Michael Oshiro <oshiro@ucsb.edu>
2023-10-10 09:51:03 -0400

    Adding progress on barrel chain

--- IntegrationTests/CombinedBarrelConfig/IRtoKF/firmware/hdl/emp_project_decl.vhd
+++ IntegrationTests/CombinedBarrelConfig/IRtoKF/firmware/hdl/emp_project_decl.vhd
@@ -19,30 +19,42 @@ package emp_project_decl is
   constant CLOCK_AUX_DIV      : clock_divisor_array_t := (18, 9, 4);  -- Dividers of CLOCK_COMMON_RATIO * 40 MHz
 
   -- Only used by nullalgo   
-  constant PAYLOAD_LATENCY : integer := 805;
+  constant PAYLOAD_LATENCY : integer := 807;
 
   constant REGION_CONF : region_conf_array_t := (
-    0      => (no_mgt, buf, no_fmt, no_buf, no_mgt),  --kDummyRegion, -- service
-    1      => (no_mgt, buf, no_fmt, no_buf, no_mgt),
-    2      => (no_mgt, buf, no_fmt, no_buf, no_mgt),
-    3      => (no_mgt, buf, no_fmt, no_buf, no_mgt),
-    4      => kDummyRegion,                        -- tcds
+    0      => kDummyRegion,                             --service/c2c
+    1      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    2      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    3      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    4      => kDummyRegion,                             --not routed
     5      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     6      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     7      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     8      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     9      => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
-    ------cross
     10     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     11     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     12     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
     13     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
-    14     => kDummyRegion,                        -- not used in apollo
-    15     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
-    16     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
-    17     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
-    18     => kDummyRegion,                        -- service
-    30     => (no_mgt, no_buf, no_fmt, buf, no_mgt),
+    14     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    15     => kDummyRegion,                             --not routed
+    ------cross
+    16     => kDummyRegion,                             --not routed
+    17     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    18     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    19     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    20     => kDummyRegion,                             --not routed
+    21     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    22     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    23     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    24     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    25     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    26     => (gty25, buf, no_fmt, no_buf, no_mgt),
+    27     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    28     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    29     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    30     => (no_mgt, no_buf, no_fmt, no_buf, no_mgt),
+    31     => kDummyRegion,                             --service/tcds
     others => kDummyRegion
     );
 
