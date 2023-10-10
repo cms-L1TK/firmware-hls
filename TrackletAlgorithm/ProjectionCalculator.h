9fe7967ecc768768adc244649d1332357cab381b
“jpearkes” <jannicke.pearkes@cern.ch>
2023-10-10 14:35:57 +0200

    Yay! Garbage being written for each tracklet

--- TrackletAlgorithm/ProjectionCalculator.h
+++ TrackletAlgorithm/ProjectionCalculator.h
@@ -12,7 +12,7 @@
 #include "TEBuffer.h"
 #include "TrackletEngineUnit.h"
 #include "TrackletProcessor.h"
-
+#include "TrackletProcessor_parameters.h"
 
 
 // This is the primary interface for the ProjectionCalculator.
@@ -36,12 +36,33 @@ template<
         TrackletProjectionMemory<DISK> projout_disk[TC::N_PROJOUT_DISK])
 {
 
-
   int TCID = 0; // TODO: unsure if this should be passed in or not 
+  // Set up writing the projections 
+  int nproj_barrel_ps[TC::N_PROJOUT_BARRELPS];//  Number of projections written to the different parts
+  int nproj_barrel_2s[TC::N_PROJOUT_BARREL2S];
+  int nproj_disk[TC::N_PROJOUT_DISK];
+  #pragma HLS array_partition variable=nproj_barrel_ps complete
+  #pragma HLS array_partition variable=nproj_barrel_2s complete
+  #pragma HLS array_partition variable=nproj_disk complete
+  for (unsigned int i = 0; i < TC::N_PROJOUT_BARRELPS; i++) {
+    #pragma HLS unroll
+    nproj_barrel_ps[i] = 0;
+  }
+  for (unsigned int i = 0; i < TC::N_PROJOUT_BARREL2S; i++) {
+    #pragma HLS unroll
+    nproj_barrel_2s[i] = 0;
+  }
+  for (unsigned int i = 0; i < TC::N_PROJOUT_DISK; i++) {
+    #pragma HLS unroll
+    nproj_disk[i] = 0;
+  }
+
 
   //for (int trackletCounter = 0, trackletCounter < 1, trackletCounter++) 
-  for (TrackletProjection<BARRELPS>::TProjTrackletIndex trackletIndex = 0; trackletIndex < 1; trackletIndex++) 
+  for (TrackletProjection<BARRELPS>::TProjTrackletIndex trackletIndex = 0; trackletIndex < 20; trackletIndex++) 
   {
+    #pragma HLS unroll
+    std::cout << "Testing -----------------------------------------------------------------" << std::endl; // Why not producing output? Had to add in generate statement
     std::cout << trackletIndex << std::endl;
   
     //TrackletProjection<BARRELPS>::TProjTrackletIndex trackletIndex = 0; // TODO: Unsure what this should be
@@ -52,7 +73,8 @@ template<
     
     // Read in tracklet parameters from the memory //
     const auto &tpar = trackletParameters->read_mem(bx, trackletIndex);
-    //std::cout<< tpar << std::endl; 
+    std::cout<< tpar.getPhi0() << std::endl; 
+    if (tpar.getPhi0() != 0){
 
     // Calculate the projections (TODO: actually calculate instead of dummies)
     TC::Types::phiL phiL = tpar.getPhi0();
@@ -60,22 +82,7 @@ template<
     TC::Types::der_phiL der_phiL = tpar.getPhi0();
     TC::Types::der_zL der_zL = tpar.getZ0();
     
-    // Set up writing the projections 
-    int nproj_barrel_ps[TC::N_PROJOUT_BARRELPS];//  Number of projections written to the different parts
-    int nproj_barrel_2s[TC::N_PROJOUT_BARREL2S];
-    int nproj_disk[TC::N_PROJOUT_DISK];
-    for (unsigned int i = 0; i < TC::N_PROJOUT_BARRELPS; i++) {
-      #pragma HLS unroll
-      nproj_barrel_ps[i] = 0;
-    }
-    for (unsigned int i = 0; i < TC::N_PROJOUT_BARREL2S; i++) {
-      #pragma HLS unroll
-      nproj_barrel_2s[i] = 0;
-    }
-    for (unsigned int i = 0; i < TC::N_PROJOUT_DISK; i++) {
-      #pragma HLS unroll
-      nproj_disk[i] = 0;
-    }
+
 
     // Create garbage projections
     const TrackletProjection<BARRELPS> tproj_L3(TCID, trackletIndex, phiL, zL, der_phiL, der_zL);
@@ -88,18 +95,22 @@ template<
     //const TrackletProjection<BARREL2S> tproj_L6(TCID, trackletIndex, phiL[3], zL[3], der_phiL, der_zL);
 
     // Write the projections // TODO: TPROJMask copy in logic for generate_PC.py
-    addL3 = TC::addProj<BARRELPS, TC::nproj_L3, (( TC::mask_L3) >> TC::shift_L3)> (tproj_L3, bx, &projout_barrel_ps[TC::L3PHIA], &nproj_barrel_ps[TC::L3PHIA], true);
-    //addL3 = TC::addProj<BARRELPS, nproj_L3, ((TPROJMaskBarrel & mask_L3) >> shift_L3)> (tproj_L3, bx, &projout_barrel_ps[L3PHIA], &nproj_barrel_ps[L3PHIA], success && valid_proj[0]);
-    addL4 = TC::addProj<BARREL2S, TC::nproj_L4, ((TC::mask_L4) >> TC::shift_L4)> (tproj_L4, bx, &projout_barrel_2s[TC::L4PHIA], &nproj_barrel_2s[TC::L4PHIA], true);
-    addL5 = TC::addProj<BARREL2S, TC::nproj_L5, ((TC::mask_L5) >> TC::shift_L5)> (tproj_L5, bx, &projout_barrel_2s[TC::L5PHIA], &nproj_barrel_2s[TC::L5PHIA], true);
-    addL6 = TC::addProj<BARREL2S, TC::nproj_L6, ((TC::mask_L6) >> TC::shift_L6)> (tproj_L6, bx, &projout_barrel_2s[TC::L6PHIA], &nproj_barrel_2s[TC::L6PHIA], true);
-    std::cout << addL3 << std::endl;
-    std::cout << "Testing -----------------------------------------------------------------" << std::endl; // Why not producing output? Had to add in generate statement
-
-    bx_o = bx; //Makes sure that bxs are aligned to make different "page" per bx in the system 
+    const uint32_t MaskBarrel = TPROJMaskBarrel<Seed, iTC>();
+    const uint32_t MaskDisk = TPROJMaskDisk<Seed, iTC>();
+    //std::cout << MaskBarrel << std::endl;
+    //addL3 = TC::addProj<BARRELPS, TC::nproj_L3, (( MaskBarrel & TC::mask_L3) >> TC::shift_L3)> (tproj_L3, bx, &projout_barrel_ps[TC::L3PHIA], &nproj_barrel_ps[TC::L3PHIA], true);
+    addL3 = TC::addProj<BARRELPS, TC::nproj_L3, ((MaskBarrel & TC::mask_L3) >> TC::shift_L3)> (tproj_L3, bx, &projout_barrel_ps[TC::L3PHIA], &nproj_barrel_ps[TC::L3PHIA], true);
+    addL4 = TC::addProj<BARREL2S, TC::nproj_L4, ((MaskBarrel & TC::mask_L4) >> TC::shift_L4)> (tproj_L4, bx, &projout_barrel_2s[TC::L4PHIA], &nproj_barrel_2s[TC::L4PHIA], true);
+    addL5 = TC::addProj<BARREL2S, TC::nproj_L5, ((MaskBarrel & TC::mask_L5) >> TC::shift_L5)> (tproj_L5, bx, &projout_barrel_2s[TC::L5PHIA], &nproj_barrel_2s[TC::L5PHIA], true);
+    addL6 = TC::addProj<BARREL2S, TC::nproj_L6, ((MaskBarrel & TC::mask_L6) >> TC::shift_L6)> (tproj_L6, bx, &projout_barrel_2s[TC::L6PHIA], &nproj_barrel_2s[TC::L6PHIA], true);
+    std::cout << addL3 <<addL4 <<addL5 << addL6 << std::endl;
+    
+  }
+    
     
 
   }
+  bx_o = bx; //Makes sure that bxs are aligned to make different "page" per bx in the system 
 }
 
 #endif
