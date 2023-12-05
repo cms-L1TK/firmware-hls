6ea0e240a19295612444b8caa54b8d473e122fc6
mlarson <mark.steven.larson@cern.ch>
2023-12-05 19:07:44 +0100

    SWLUTReader update
    Conflicts:
    	TrackletAlgorithm/SWLUTReader.h

--- TrackletAlgorithm/SWLUTReader.h
+++ TrackletAlgorithm/SWLUTReader.h
@@ -1,17 +1,25 @@
 #include <fstream>
 #include <string>
+#include <mutex>
+
+// Define a global mutex for locking multi-threading
+std::mutex readSWLUTMutex_;
+
 // The following function provides a more compiler efficient method for reading
 // look up table arrays (currently used in TP), this is only used in C simulations and not synthesis
 template<class lutType, int lutsize> bool readSWLUT(lutType lut[lutsize],const std::string& lutpath, bool skipfirst = true, const bool hex = false){
-  //std::cout << "I'm reading : "<< lutpath <<std::endl;
+  std::lock_guard<std::mutex> lock(readSWLUTMutex_); // Lock the mutex
   const std::string emdatapath =
 #ifdef CMSSW_GIT_HASH //at run time the working directory differs between CSIM and future SW
   "../../../../../firmware-hls/emData/";
+  const std::string fullpath = emdatapath + lutpath;
+  edm::FileInPath LUTFileInPath(fullpath);
+  std::ifstream LUTif(LUTFileInPath.fullPath());
 #else
   "../../../../../emData/";
-#endif
   const std::string fullpath = emdatapath + lutpath;
   std::ifstream LUTif(fullpath);
+#endif
   if (hex) {
     LUTif >> std::hex;
   }
