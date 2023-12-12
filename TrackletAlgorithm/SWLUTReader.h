4479583c8edcd0722766609cffacd5c9a673faaf
Jason Fan <jasonfan393@gmail.com>
2023-12-12 09:08:35 -0500

    update to address comments

--- TrackletAlgorithm/SWLUTReader.h
+++ TrackletAlgorithm/SWLUTReader.h
@@ -2,15 +2,15 @@
 #include <string>
 // The following function provides a more compiler efficient method for reading
 // look up table arrays (currently used in TP), this is only used in C simulations and not synthesis
-template<class lutType, int lutsize> bool readSWLUT(lutType lut[lutsize], std::string lutpath, bool skipfirst = true, bool hex = false){
+template<class lutType, int lutsize> bool readSWLUT(lutType lut[lutsize],const std::string lutpath, bool skipfirst = true, const bool hex = false){
   //std::cout << "I'm reading : "<< lutpath <<std::endl;
-  std::string emdatapath =
-#ifdef __CSIM__ //at run time the working directory differs between CSIM and future SW
-  "../../../../../emData/";
-#else
+  const std::string emdatapath =
+#ifdef CMSSW_GIT_HASH //at run time the working directory differs between CSIM and future SW
   "../../../../../firmware-hls/emData/";
+#else
+  "../../../../../emData/";
 #endif
-  std::string fullpath = emdatapath + lutpath;
+  const std::string fullpath = emdatapath + lutpath;
   std::ifstream LUTif(fullpath);
   if (hex) {
     LUTif >> std::hex;
