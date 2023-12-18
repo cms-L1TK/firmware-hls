34104ba6b446993f241edb03799397180d4ddad5
Michael Oshiro <oshiro@ucsb.edu>
2023-12-17 20:14:59 -0500

    Copied fixed secproctolink.vhd and fwtosim_comparison.py from other branch

--- IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/scripts/fwtosim_comparison.py
+++ IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/scripts/fwtosim_comparison.py
@@ -17,21 +17,25 @@ with open(args.sim_file) as sim_file:
 
 # Data manipulation for EMP output file
 checks = ['0v', '1v', 'Uv']
-words_per_event = 5
+words_per_event = 108
+hex_chars_per_track = 80
 
 elines = [eline for eline in elines if 'Frame' in eline] #Remove EMP header
-elines = [eline.split(': ')[1] for eline in elines] #Remove Frame thing
 nchar = len(elines[0]) - 1 #Number of characters per line
 elines = [eline[0:nchar] for eline in elines] #Remove final '\n' from line
-elines = [eline.split(' ') for eline in elines]
-elines = [eline[:1] for eline in elines] # only keep link 1
-elines = [eline for sublist in elines for eline in sublist] #Recreate list
-elines = [eline for eline in elines if '1v' in eline] #Only keep valid events
-elines = [eline.replace('1v', '') for eline in elines] #Remove '1v' sideband info
-elines = [elines[x:x+words_per_event] for x in range(0, len(elines), 5)] #Create sublists with events
+elines = [eline.split() for eline in elines]
+#elines = [eline[3] for eline in elines if eline[2][3]=='1'] # only keep link 0 and keep when valid bit set
+elines = [eline[3] if eline[2][3]=='1' else '' for eline in elines] # only keep link 0 and keep when valid bit set
+elines = [elines[x:x+words_per_event] for x in range(0, len(elines), words_per_event)] #Create sublists with events
 elines = [''.join(sub_list) for sub_list in elines] #Merge single events
+elines = [eline for eline in elines if eline!=''] #remove empty events for now
+elines = [[eline[x:x+hex_chars_per_track] for x in range(0,len(eline),hex_chars_per_track)] for eline in elines] #split tracks within events
+elines = [track for event in elines for track in event] #flatten
 elines = [eline.upper() for eline in elines] #Change to capital letters
-elines = [eline[11:] for eline in elines] # Remove link header
+elines = [eline.replace('8048D15A','2')[:-4] for eline in elines] #remove header and tailer
+print('DEBUG: elines')
+print(elines)
+print('DEBUG: elines')
 
 slines = [sline for sline in slines if '0x' in sline] #Remove sim header
 slines = [sline.split() for sline in slines]
@@ -39,6 +43,9 @@ slines = [sline[4:] for sline in slines]
 slines = [sline for sublist in slines for sline in sublist] #Recreate list
 slines = [sline.replace('0x','') for sline in slines]
 slines = [sline[:69] for sline in slines]
+print('DEBUG: slines')
+print(slines)
+print('DEBUG: slines')
 
 events = min(len(elines), len(slines))
 mismatches = 0
@@ -51,3 +58,4 @@ if (mismatches == 0):
     print("Test passed!! {} events processed".format(events))
 else:
     print("Test not passed!!")
+    print("{} of {} tracks incorrect.".format(mismatches, events))
