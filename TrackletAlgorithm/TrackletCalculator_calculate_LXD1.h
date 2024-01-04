4114db4758da430f101794fcdb9fc6f139832a4e
Anders Ryd <ar322@cornell.edu>
2024-01-04 00:05:52 -0500

    First commit of code that removes the iMath calculations for tracklet parameters and projections

--- TrackletAlgorithm/TrackletCalculator_calculate_LXD1.h
+++ TrackletAlgorithm/TrackletCalculator_calculate_LXD1.h
@@ -1,9 +1,25 @@
 //
-// This code is auto generated in the emulation by imath and should
-// not be modified
-//
 #include "ap_int.h"
 #include "Constants.h"
+
+static void init_idr_overlap(ap_uint<18> *lut) {
+
+  for(unsigned int idr=0; idr<1024; idr++){
+    if (idr<10) {
+      lut[idr] = 0;
+    } else {
+      lut[idr] = (1<<n_Deltar_Overlap_)/idr;
+    }
+  }
+
+}
+
+static ap_uint<18> lut_idrinv_overlap(ap_uint<10> idr){
+  ap_uint<18> LUT_idrinv_[1024];
+  init_idr_overlap(LUT_idrinv_);
+  return LUT_idrinv_[idr];
+}
+
 template<TF::seed Seed, regionType InnerRegion, regionType OuterRegion>
 void TC::calculate_LXD1 (
   const typename AllStub<InnerRegion>::ASR r1_input,
@@ -12,15 +28,15 @@ void TC::calculate_LXD1 (
   const typename AllStub<OuterRegion>::ASR r2_input,
   const typename AllStub<OuterRegion>::ASPHI phi2_input,
   const typename AllStub<OuterRegion>::ASZ z2_input,
-  const TC::Types::rmean r1mean_input,
-  const TC::Types::zmean z2mean_input,
-  const TC::Types::rmean rproj0_input,
-  const TC::Types::rmean rproj1_input,
-  const TC::Types::rmean rproj2_input,
-  const TC::Types::zmean  zproj0_input,
-  const TC::Types::zmean  zproj1_input,
-  const TC::Types::zmean  zproj2_input,
-  const TC::Types::zmean  zproj3_input,
+  //const TC::Types::rmean r1mean_input,
+  //const TC::Types::zmean z2mean_input,
+  //const TC::Types::rmean rproj0_input,
+  //const TC::Types::rmean rproj1_input,
+  //const TC::Types::rmean rproj2_input,
+  //const TC::Types::zmean  zproj0_input,
+  //const TC::Types::zmean  zproj1_input,
+  //const TC::Types::zmean  zproj2_input,
+  //const TC::Types::zmean  zproj3_input,
 
   bool * const valid_radii,
   TC::Types::rinv * const rinv_output,
@@ -48,600 +64,118 @@ void TC::calculate_LXD1 (
 {
 #pragma HLS pipeline II=1
 #pragma HLS latency max=25
-//
-// calculating rinv_final
-//
-//
-// STEP 0
-
-// 10 bits 	 2^(-7)	0.0078125
-static const ap_int<10> plus2 = 256;
-// units 2^(0)Kr^(1)	0.0292969
-const ap_int<8> r1 = ap_int<8>(r1_input)<<1;
-// 13 bits 	 2^(0)Kr^(1)	0.0292969
-// units 2^(0)Kphi^(1)	7.84121e-06
-const ap_int<18> phi2 = ap_int<18>(phi2_input)<<3;
-// units 2^(0)Kphi^(1)	7.84121e-06
-const ap_int<18> phi1 = ap_int<18>(phi1_input)<<3;
-// units 2^(0)Kr^(1)	0.0292969
-const ap_int<13> r2 = ap_uint<12>(r2_input); //cast r2 as unsigned since all disk radii are positive only
-//
-// STEP 1
-
-// 13 bits 	 2^(0)Kr^(1)	0.0292969
-const ap_int<13> r1abs = r1 + r1mean_input;
-// 16 bits 	 2^(0)Kphi^(1)	7.84121e-06
-const ap_int<16> dphi = phi2 - phi1;
-//
-// STEP 2
-
-// 12 bits 	 2^(0)Kr^(1)	0.0292969
-const ap_int<11> dr = r2 - r1abs;
-//
-// STEP 3
-//protection from wrong radii
-*valid_radii = dr > (floatToInt(1.5,kr)); //FIXME this 1.5 is a magic number in emulation too
-
-bool negDisk = z2mean_input<=0;
-
-const ap_uint<11> addr_drinv = dr & 2047; // address for the LUT
-
-ap_int<19> drinv = negDisk ? getDRinvBackwardLUT<Seed>()[addr_drinv] : getDRinvForwardLUT<Seed>()[addr_drinv];
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-// 18 bits 	 2^(-9)Kphi^(1)Kr^(-1)	5.22747e-07
-const ap_int<32> delta0_tmp = dphi * drinv;
-const ap_int<18> delta0 = delta0_tmp >> 14;
-//
-// STEP 6
-
-// 18 bits 	 2^(4)Kphi^(1)Kr^(0)	0.000125459
-const ap_int<31> delta1_tmp = r1abs * delta0;
-const ap_int<18> delta1 = delta1_tmp >> 13;
-// 18 bits 	 2^(4)Kphi^(1)Kr^(0)	0.000125459
-const ap_int<31> delta2_tmp = r2 * delta0;
-const ap_int<18> delta2 = delta2_tmp >> 13;
-//
-// STEP 7
-
-// 18 bits 	 2^(19)Kphi^(2)Kr^(0)	3.22356e-05
-const ap_int<29> a2a_tmp = delta1 * delta2;
-const ap_int<18> a2a = a2a_tmp >> 11;
-//
-// STEP 8
-
-// 18 bits 	 2^(-14)	6.10352e-05
-const ap_int<35> a2b_tmp = (a2a * 69225);
-const ap_int<18> a2b = a2b_tmp >> 17;
-//
-// STEP 9
-
-// 17 bits 	 2^(-14)	6.10352e-05
-const ap_int<17> a2 = (ap_int<17>(plus2)<<7) - a2b;
-//
-// STEP 10
-
-// 17 bits 	 2^(-14)	6.10352e-05
-const ap_int<17> a2n = -a2;
-;
-//
-// STEP 11
-
-// 18 bits 	 2^(-9)Kphi^(1)Kr^(-1)	5.22747e-07
-const ap_int<32> rinv_tmp = a2n * delta0;
-const ap_int<18> rinv = rinv_tmp >> 14;
-//
-// STEP 12
-
-// 17 bits 	 2^(-8)Kphi^(1)Kr^(-1)	1.04549e-06
-const ap_int<17> rinv_final = rinv >> 1;
-
-//
-// calculating phi0_final
-//
-//
-// STEP 0
-
-// 10 bits 	 2^(-8)	0.00390625
-static const ap_int<10> minus1 = -256;
-//
-// STEP 1
-
-//
-// STEP 2
-
-// 14 bits 	 2^(0)Kr^(1)	0.0292969
-const ap_int<14> Rabs = r1abs + r2;
-//
-// STEP 3
-
-// 14 bits 	 2^(-2)Kr^(1)	0.00732422
-const ap_int<26> R6_tmp = (Rabs * 2730);
-const ap_int<14> R6 = R6_tmp >> 12;
-//
-// STEP 4
-
-//
-// STEP 5
-
-//
-// STEP 6
-
-// 18 bits 	 2^(3)Kphi^(1)Kr^(0)	6.27297e-05
-const ap_int<32> x4_tmp = R6 * delta0;
-const ap_int<18> x4 = x4_tmp >> 14;
-//
-// STEP 7
-
-// 18 bits 	 2^(17)Kphi^(2)Kr^(0)	8.05891e-06
-const ap_int<28> x6a_tmp = delta2 * x4;
-const ap_int<18> x6a = x6a_tmp >> 10;
-//
-// STEP 8
-
-// 18 bits 	 2^(-16)	1.52588e-05
-const ap_int<35> x6b_tmp = (x6a * 69225);
-const ap_int<18> x6b = x6b_tmp >> 17;
-//
-// STEP 9
-
-// 18 bits 	 2^(-15)	3.05176e-05
-const ap_int<19> x6m_tmp = (ap_int<19>(minus1)<<8) + x6b;
-const ap_int<18> x6m = x6m_tmp >> 1;
-//
-// STEP 10
-
-// 18 bits 	 2^(0)Kphi^(1)Kr^(0)	7.84121e-06
-const ap_int<29> phi0a_tmp = delta1 * x6m;
-const ap_int<18> phi0a = phi0a_tmp >> 11;
-//
-// STEP 11
-
-// 18 bits 	 2^(1)Kphi^(1)	1.56824e-05
-const ap_int<19> phi0_tmp = phi1 + phi0a;
-const ap_int<18> phi0_final = phi0_tmp >> 1;
-//
-// STEP 12
-
-// 18 bits 	 2^(1)Kphi^(1)	1.56824e-05
-
-//
-// calculating t_final
-//
-//
-// STEP 0
-
-// units 2^(0)Kz^(1)	0.0585938
-const ap_int<7> z2 = ap_int<7>(z2_input);
-// 14 bits 	 2^(0)Kz^(1)	0.0585938
-// units 2^(0)Kz^(1)	0.0585938
-const ap_int<12> z1 = ap_int<12>(z1_input);
-//
-// STEP 1
-
-// 14 bits 	 2^(0)Kz^(1)	0.0585938
-const ap_int<14> z2abs = z2 + z2mean_input;
-//
-// STEP 2
-
-// 12 bits 	 2^(0)Kz^(1)	0.0585938
-const ap_int<12> dz = z2abs - z1;
-//
-// STEP 3
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-// 18 bits 	 2^(-14)Kr^(-1)Kz^(1)	0.00012207
-const ap_int<27> deltaZ_tmp = dz * drinv;
-const ap_int<18> deltaZ = deltaZ_tmp >> 9;
-//
-// STEP 6
-
-//
-// STEP 7
-
-//
-// STEP 8
-
-//
-// STEP 9
-
-//
-// STEP 10
-
-// 16 bits 	 2^(-14)	6.10352e-05
-const ap_int<16> a = a2>>1;
-;
-// 18 bits 	 2^(-15)Kr^(-1)Kz^(1)	6.10352e-05
-const ap_int<31> t_tmp = a * deltaZ;
-const ap_int<18> t = t_tmp >> 13;
-//
-// STEP 11
-
-// 13 bits 	 2^(-10)Kr^(-1)Kz^(1)	0.00195312
-const ap_int<14> t_final = t>>5;
-//
-// calculating z0_final
-//
-//
-// STEP 0
-
-//
-// STEP 1
-
-//
-// STEP 2
-
-//
-// STEP 3
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-//
-// STEP 6
-
-// 18 bits 	 2^(-4)Kr^(0)Kz^(1)	0.00366211
-const ap_int<28> z0a_tmp = r1abs * deltaZ;
-const ap_int<18> z0a = z0a_tmp >> 10;
-//
-// STEP 7
-
-//
-// STEP 8
-
-//
-// STEP 9
-
-//
-// STEP 10
-
-// 18 bits 	 2^(-4)Kr^(0)Kz^(1)	0.00366211
-const ap_int<33> z0b_tmp = z0a * x6m;
-const ap_int<18> z0b = z0b_tmp >> 15;
-//
-// STEP 11
-
-// 18 bits 	 2^(-4)Kz^(1)	0.00366211
-const ap_int<18> z0 = (ap_int<18>(z1)<<4) + z0b;
-//
-// STEP 12
-
-// 14 bits 	 2^(0)Kz^(1)	0.0585938
-const ap_int<14> z0_final = z0 >> 4;
-
-//
-// calculating phiL_0_final
-//
-//
-// STEP 0
-
-// units 2^(0)Kr^(1)	0.0292969
-// 10 bits 	 2^(-8)	0.00390625
-static const ap_int<10> plus1 = 256;
-//
-// STEP 1
-
-//
-// STEP 2
-
-//
-// STEP 3
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-//
-// STEP 6
-
-// 17 bits 	 2^(-9)Kphi^(1)Kr^(-1)	5.22747e-07
-const ap_int<17> x2 = delta0>>1;
-;
-// 18 bits 	 2^(3)Kphi^(1)Kr^(0)	6.27297e-05
-const ap_int<30> x1_0_tmp = x2 * rproj0_input;
-const ap_int<18> x1_0 = x1_0_tmp >> 12;
-//
-// STEP 7
-
-//
-// STEP 8
-
-//
-// STEP 9
-
-//
-// STEP 10
-
-//
-// STEP 11
-
-// 18 bits 	 2^(1)Kphi^(1)Kr^(0)	1.56824e-05
-const ap_int<30> x8_0_tmp = x1_0 * a2n;
-const ap_int<18> x8_0 = x8_0_tmp >> 12;
-//
-// STEP 12
-
-// 18 bits 	 2^(20)Kphi^(2)Kr^(0)	6.44712e-05
-const ap_int<36> x12_0_tmp = x8_0 * x8_0;
-const ap_int<18> x12_0 = x12_0_tmp >> 18;
-//
-// STEP 13
-
-// 18 bits 	 2^(-13)	0.00012207
-const ap_int<35> x12A_0_tmp = (x12_0 * 69225);
-const ap_int<18> x12A_0 = x12A_0_tmp >> 17;
-//
-// STEP 14
-
-// 18 bits 	 2^(-15)	3.05176e-05
-const ap_int<35> x20_0_tmp = (x12A_0 * 87381);
-const ap_int<18> x20_0 = x20_0_tmp >> 17;
-//
-// STEP 15
-
-// 18 bits 	 2^(-14)	6.10352e-05
-const ap_int<19> x10_0_tmp = (ap_int<19>(plus1)<<7) + x20_0;
-const ap_int<18> x10_0 = x10_0_tmp >> 1;
-//
-// STEP 16
-
-// 18 bits 	 2^(1)Kphi^(1)Kr^(0)	1.56824e-05
-const ap_int<32> x22_0_tmp = x8_0 * x10_0;
-const ap_int<18> x22_0 = x22_0_tmp >> 14;
-//
-// STEP 17
-
-// 19 bits 	 2^(1)Kphi^(1)	1.56824e-05
-const ap_int<19> phiL_0 = phi0_final - x22_0;
-//
-// STEP 18
-
-// 20 bits 	 2^(0)Kphi^(1)	7.84121e-06
-const ap_int<20> phiL_0_final = phiL_0 << 1;
-
-//
-// calculating phiL_1_final
-//
-//
-// STEP 0
-
-// units 2^(0)Kr^(1)	0.0292969
-//
-// STEP 1
-
-//
-// STEP 2
-
-//
-// STEP 3
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-//
-// STEP 6
-
-// 18 bits 	 2^(3)Kphi^(1)Kr^(0)	6.27297e-05
-const ap_int<30> x1_1_tmp = x2 * rproj1_input;
-const ap_int<18> x1_1 = x1_1_tmp >> 12;
-//
-// STEP 7
-
-//
-// STEP 8
-
-//
-// STEP 9
-
-//
-// STEP 10
-
-//
-// STEP 11
-
-// 18 bits 	 2^(1)Kphi^(1)Kr^(0)	1.56824e-05
-const ap_int<30> x8_1_tmp = x1_1 * a2n;
-const ap_int<18> x8_1 = x8_1_tmp >> 12;
-//
-// STEP 12
-
-// 18 bits 	 2^(20)Kphi^(2)Kr^(0)	6.44712e-05
-const ap_int<36> x12_1_tmp = x8_1 * x8_1;
-const ap_int<18> x12_1 = x12_1_tmp >> 18;
-//
-// STEP 13
-
-// 18 bits 	 2^(-13)	0.00012207
-const ap_int<35> x12A_1_tmp = (x12_1 * 69225);
-const ap_int<18> x12A_1 = x12A_1_tmp >> 17;
-//
-// STEP 14
-
-// 18 bits 	 2^(-15)	3.05176e-05
-const ap_int<35> x20_1_tmp = (x12A_1 * 87381);
-const ap_int<18> x20_1 = x20_1_tmp >> 17;
-//
-// STEP 15
-
-// 18 bits 	 2^(-14)	6.10352e-05
-const ap_int<19> x10_1_tmp = (ap_int<19>(plus1)<<7) + x20_1;
-const ap_int<18> x10_1 = x10_1_tmp >> 1;
-//
-// STEP 16
-
-// 18 bits 	 2^(1)Kphi^(1)Kr^(0)	1.56824e-05
-const ap_int<32> x22_1_tmp = x8_1 * x10_1;
-const ap_int<18> x22_1 = x22_1_tmp >> 14;
-//
-// STEP 17
-
-// 19 bits 	 2^(1)Kphi^(1)	1.56824e-05
-const ap_int<19> phiL_1 = phi0_final - x22_1;
-//
-// STEP 18
-
-// 20 bits 	 2^(0)Kphi^(1)	7.84121e-06
-const ap_int<20> phiL_1_final = phiL_1 << 1;
-
-//
-// calculating phiL_2_final
-//
-//
-// STEP 0
-
-// units 2^(0)Kr^(1)	0.0292969
-//
-// STEP 1
-
-//
-// STEP 2
-
-//
-// STEP 3
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-//
-// STEP 6
-
-// 18 bits 	 2^(3)Kphi^(1)Kr^(0)	6.27297e-05
-const ap_int<30> x1_2_tmp = x2 * rproj2_input;
-const ap_int<18> x1_2 = x1_2_tmp >> 12;
-//
-// STEP 7
-
-//
-// STEP 8
-
-//
-// STEP 9
-
-//
-// STEP 10
-
-//
-// STEP 11
-
-// 18 bits 	 2^(1)Kphi^(1)Kr^(0)	1.56824e-05
-const ap_int<30> x8_2_tmp = x1_2 * a2n;
-const ap_int<18> x8_2 = x8_2_tmp >> 12;
-//
-// STEP 12
-
-// 18 bits 	 2^(20)Kphi^(2)Kr^(0)	6.44712e-05
-const ap_int<36> x12_2_tmp = x8_2 * x8_2;
-const ap_int<18> x12_2 = x12_2_tmp >> 18;
-//
-// STEP 13
-
-// 18 bits 	 2^(-13)	0.00012207
-const ap_int<35> x12A_2_tmp = (x12_2 * 69225);
-const ap_int<18> x12A_2 = x12A_2_tmp >> 17;
-//
-// STEP 14
-
-// 18 bits 	 2^(-15)	3.05176e-05
-const ap_int<35> x20_2_tmp = (x12A_2 * 87381);
-const ap_int<18> x20_2 = x20_2_tmp >> 17;
-//
-// STEP 15
-
-// 18 bits 	 2^(-14)	6.10352e-05
-const ap_int<19> x10_2_tmp = (ap_int<19>(plus1)<<7) + x20_2;
-const ap_int<18> x10_2 = x10_2_tmp >> 1;
-//
-// STEP 16
-
-// 18 bits 	 2^(1)Kphi^(1)Kr^(0)	1.56824e-05
-const ap_int<32> x22_2_tmp = x8_2 * x10_2;
-const ap_int<18> x22_2 = x22_2_tmp >> 14;
-//
-// STEP 17
-
-// 19 bits 	 2^(1)Kphi^(1)	1.56824e-05
-const ap_int<19> phiL_2 = phi0_final - x22_2;
-//
-// STEP 18
-
-// 20 bits 	 2^(0)Kphi^(1)	7.84121e-06
-const ap_int<20> phiL_2_final = phiL_2 << 1;
-
-//
-// calculating zL_0_final
-//
-//
-// STEP 0
-
-//
-// STEP 1
-
-//
-// STEP 2
-
-//
-// STEP 3
-
-//
-// STEP 4
-
-//
-// STEP 5
-
-//
-// STEP 6
-
-//
-// STEP 7
-
-//
-// STEP 8
-
-//
-// STEP 9
-
-//
-// STEP 10
-
-//
-// STEP 11
-
-// 18 bits 	 2^(-2)Kr^(0)Kz^(1)	0.0146484
-const ap_int<31> x11_0_tmp = rproj0_input * t;
-const ap_int<18> x11_0 = x11_0_tmp >> 13;
-//
-// STEP 12
-
-//
-// STEP 13
-
-//
-// STEP 14
-
-//
-// STEP 15
 
+  //First convert input to right bits
+  ap_uint<12> ir1c = (ap_int<10>(r1_input) << 1) + rmean[seedLayers[Seed][0]];
+  ap_uint<12> ir2c = r2_input;
+
+  const ap_int<18> phi1c = ap_int<18>(phi1_input)<<3;
+  const ap_int<18> phi2c = ap_int<18>(phi2_input)<<3;
+
+  const TC::Types::zmean z2mean = zmean[seedLayers[Seed][1]];
+
+  const ap_int<14> z1c = z1_input;
+  const ap_int<14> z2c = ap_int<7>(z2_input) + (z1c > 0 ? ap_int<14>(z2mean) : ap_int<14>(-z2mean));
+
+  ap_int<12> idz = z2c - z1c;
+
+  ap_uint<12> idr = (ap_int<12>(ir2c - ir1c));
+  ap_uint<18> invdr = lut_idrinv_overlap(idr);
+
+  *valid_radii = idr > floatToInt(1.5,kr); //FIXME this 1.5 is a magic number in emulation too
+
+  ap_int<18> idelta0 = ((phi2c - phi1c)*invdr) >> n_delta0_Overlap_;
+  ap_int<18> ideltaz = (idz*invdr) >> n_deltaz_Overlap_;
+
+  ap_int<20> idelta1 = (ir1c*idelta0) >> n_delta1_;  
+  ap_int<20> idelta2 = (ir2c*idelta0) >> n_delta2_;
+
+  ap_int<20> idelta12 = (idelta1*idelta2) >> n_delta12_;
+
+  constexpr int iHG = phiHG_*phiHG_*(1 << n_HG_);
+
+  ap_uint<n_a_Overlap_+1> ia = ((1 << n_a_Overlap_) - ((idelta12*iHG) >> (2*n_Deltar_Overlap_ + 2*n_phi_ + n_HG_ - 2*n_delta0_Overlap_ - n_delta1_ - n_delta2_ - n_delta12_ + 1 - n_a_Overlap_)));
+
+  constexpr int ir6fact = (1<<n_r6_Overlap_)*phiHG_*phiHG_/6.0;
+  ap_int<24> ir6 = (ir1c+ir2c)*ir6fact;
+
+  ap_int<18> idelta02 = (idelta0*idelta2) >> n_delta02_;
+  
+  ap_int<n_x6_+2> ix6 = (-(1 << n_x6_)+((ir6*idelta02) >> (n_r6_Overlap_ + 2*n_Deltar_Overlap_ + 2*n_phi_ - n_x6_ - n_delta2_- n_delta02_ - 2*n_delta0_Overlap_)));
+  
+  ap_int<18> it1 = (ir1c*ideltaz) >> (n_Deltar_Overlap_ - n_deltaz_Overlap_);
+
+  
+  ap_int<15> irinv_new = ((-idelta0*ia) >> (n_phi_ + n_a_Overlap_ - n_rinv_ + n_Deltar_Overlap_ - n_delta0_Overlap_ - n_r_ - 1));
+
+  ap_int<18> iphi0_new = (phi1c >> (n_phi_ - n_phi0_))+((idelta1*ix6) >> (n_Deltar_Overlap_ + n_x6_ + n_phi_ - n_delta0_Overlap_ - n_delta1_ - n_phi0_));
+
+  ap_int<14> it_new = ((ideltaz*ia) >> (n_Deltar_Overlap_ + n_a_Overlap_ + n_z_ - n_t_ - n_deltaz_Overlap_ - n_r_));
+
+  ap_int<11> iz0_new = z1c+((it1*ix6) >> n_x6_);
+  
+  std::cout << "pars : " << irinv_new << " " << iphi0_new << " " << iz0_new << " " << it_new << std::endl; 
+
+
+  constexpr int irproj0 = rmean[projectionLayers[Seed][0]];
+  constexpr int irproj1 = rmean[projectionLayers[Seed][1]];
+  constexpr int irproj2 = rmean[projectionLayers[Seed][2]];
+
+  constexpr int irprojtilde0 = irproj0*phiHG_/sqrtsix + 0.5;
+  constexpr int irprojtilde1 = irproj1*phiHG_/sqrtsix + 0.5;
+  constexpr int irprojtilde2 = irproj2*phiHG_/sqrtsix + 0.5;
+
+  const ap_int<13> rproj0 = irproj0;
+  const ap_int<13> rproj1 = irproj1;
+  const ap_int<13> rproj2 = irproj2;
+
+  const ap_int<13> rprojtilde0 = irprojtilde0;
+  const ap_int<13> rprojtilde1 = irprojtilde1;
+  const ap_int<13> rprojtilde2 = irprojtilde2;
+  
+  projToLayer(rproj0, rprojtilde0, irinv_new, iphi0_new, it_new, iz0_new, *zL_0_output, *phiL_0_output);
+  projToLayer(rproj1, rprojtilde1, irinv_new, iphi0_new, it_new, iz0_new, *zL_1_output, *phiL_1_output);
+  projToLayer(rproj2, rprojtilde2, irinv_new, iphi0_new, it_new, iz0_new, *zL_2_output, *phiL_2_output);
+  
+  *der_phiL_output = -(irinv_new >> (1+3));
+  *der_zL_output = it_new >> 3;
+
+  ap_uint<20> itinv = lut_itinv(abs(it_new)&4095);
+
+  constexpr int izproj0 = zmean[projectionDisks[Seed][0]];
+  constexpr int izproj1 = zmean[projectionDisks[Seed][1]];
+  constexpr int izproj2 = zmean[projectionDisks[Seed][2]];
+  constexpr int izproj3 = zmean[projectionDisks[Seed][3]];
+
+  const ap_int<14> zproj0 = izproj0;
+  const ap_int<14> zproj1 = izproj1;
+  const ap_int<14> zproj2 = izproj2;
+  const ap_int<14> zproj3 = izproj3;
+
+  projToDisk(zproj0, itinv, irinv_new, iphi0_new, it_new, iz0_new, *rD_0_output, *phiD_0_output);
+  projToDisk(zproj1, itinv, irinv_new, iphi0_new, it_new, iz0_new, *rD_1_output, *phiD_1_output);
+  projToDisk(zproj2, itinv, irinv_new, iphi0_new, it_new, iz0_new, *rD_2_output, *phiD_2_output);
+  projToDisk(zproj3, itinv, irinv_new, iphi0_new, it_new, iz0_new, *rD_3_output, *phiD_3_output);
+
+  *der_phiD_output = (-irinv_new*itinv) >> 17;
+  *der_rD_output = itinv >> 5;
+  
+  if (it_new < 0 ) {
+    *der_phiD_output = - *der_phiD_output;
+    *der_rD_output =  - *der_rD_output;
+  }
+
+  //
+  // wiring the outputs 
+  //
+  *rinv_output = irinv_new;
+  *phi0_output = iphi0_new;
+  *t_output = it_new;
+  *z0_output = iz0_new;
+
+
+
+<<<<<<< HEAD
 //
 // STEP 16
 
@@ -1656,4 +1190,6 @@ const ap_int<8> der_rD_final = invt >> 10;
 *rD_3_output = rD_3_final;
 *der_phiD_output = der_phiD_final;
 *der_rD_output = der_rD_final;
+=======
+>>>>>>> First commit of code that removes the iMath calculations for tracklet parameters and projections
 }
