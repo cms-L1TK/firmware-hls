4908f402d2c7f315a08e584ae68eeb33649ad8d6
Jason Fan <jasonfan393@gmail.com>
2024-02-13 12:11:34 -0500

    remove pragma

--- TrackletAlgorithm/MatchProcessor.h
+++ TrackletAlgorithm/MatchProcessor.h
@@ -1240,7 +1240,6 @@ void MatchProcessor(BXType bx,
 
   MatchEngineUnit<VMSMEType, kNbitsrzbinMP, VMPTYPE, APTYPE, LAYER, ASTYPE> matchengine[kNMatchEngines];
 #pragma HLS ARRAY_PARTITION variable=matchengine complete
-#pragma HLS ARRAY_PARTITION variable=instubdata complete dim=1
 #pragma HLS ARRAY_PARTITION variable=projin dim=1
 #pragma HLS ARRAY_PARTITION variable=numbersin complete
   
