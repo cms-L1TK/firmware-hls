b6247f159075ff8ef6e77b8521129e2f699fb1da
Peter Wittich <wittich@cornell.edu>
2024-03-09 09:09:46 -0500

    working build for F2 with trivial payload (CSP test)

--- IntegrationTests/DualFPGA/firmware/scripts/setup_project.sh
+++ IntegrationTests/DualFPGA/firmware/scripts/setup_project.sh
@@ -16,15 +16,15 @@ ipbb add git https://gitlab.cern.ch/ttc/legacy_ttc.git -b v2.1
 ipbb add git ssh://git@gitlab.cern.ch:7999/cms-tcds/cms-tcds2-firmware.git -b v0_1_1
 ipbb add git ssh://git@gitlab.cern.ch:7999/hptd/tclink.git -r fda0bcf
 ipbb add git https://github.com/ipbus/ipbus-firmware -b v1.9
-#ipbb add git ssh://git@gitlab.cern.ch:7999/dth_p1-v2/slinkrocket_ips.git -b v03.12
-#ipbb add git ssh://git@gitlab.cern.ch:7999/dth_p1-v2/slinkrocket.git -b v03.12
-ipbb add git https://github.com/cms-l1tk/firmware-hls.git -b dual_fpga
+ipbb add git ssh://git@gitlab.cern.ch:7999/dth_p1-v2/slinkrocket_ips.git -b v03.12
+ipbb add git ssh://git@gitlab.cern.ch:7999/dth_p1-v2/slinkrocket.git -b v03.12
+ipbb add git ssh://git@github.com/cms-l1tk/firmware-hls.git -b dual_fpga
 
 export PATH=/opt/cactus/bin/uhal/tools:$PATH 
 export LD_LIBRARY_PATH=/opt/cactus/lib:${LD_LIBRARY_PATH}
 
 export COLUMNS=120
-ipbb proj create vivado tf_p1 firmware-hls:IntegrationTests/DualFPGA/firmware/cfg apollo_f1.dep
+ipbb proj create vivado tf_p1 firmware-hls:IntegrationTests/DualFPGA/ apollo_f1.dep
 cd proj/tf_p1/
 ipbb ipbus gendecoders
 ipbb vivado generate-project --single
