5a46dc4c36c1578b3748196d42edccfd3723e689
Anders Ryd <ar322@cornell.edu>
2024-03-19 20:03:13 -0400

    Temporarily add config for second FPGA project

--- emData/download.sh
+++ emData/download.sh
@@ -3,12 +3,9 @@ set -e
 
 #### generated with commit 6e61462 ####
 # Combined modules
-<<<<<<< HEAD
 memprints_url_cm="https://aryd.web.cern.ch/aryd/MemPrints_Combined_240310.tgz"
-=======
-memprints_url_cm="https://aryd.web.cern.ch/aryd/MemPrints_Combined_240202.tgz"
->>>>>>> 385dfad (Update the MemPrints files as the doMultupleMatches was not set false in the earlier version)
 luts_url_cm="https://aryd.web.cern.ch/aryd/LUTs_Combined_231208.tgz"
+#memprints_url_cm="https://aryd.web.cern.ch/aryd/MemPrints_Combined_240202.tgz"
 # Reduced Combined modules
 memprints_url_reducedcm="https://aryd.web.cern.ch/aryd/MemPrints_CMReduced_240202.tgz"
 luts_url_reducedcm="https://aryd.web.cern.ch/aryd/LUTs_CMReduced_240121.tgz"
@@ -138,6 +135,9 @@ cp ../LUTsCM2/memorymodules.dat reducedcm2_memorymodules.dat
 cp ../LUTsCMBarrel/wires.dat cmbarrel_wires.dat
 cp ../LUTsCMBarrel/processingmodules.dat cmbarrel_processingmodules.dat
 cp ../LUTsCMBarrel/memorymodules.dat cmbarrel_memorymodules.dat
+cp ../FPGA2_Config/wires.dat fpga2_wires.dat
+cp ../FPGA2_Config/memorymodules.dat fpga2_memorymodules.dat
+cp ../FPGA2_Config/processingmodules.dat fpga2_processingmodules.dat
 
 ./makeReducedConfig.py --no-graph -t "TP" -s "C" -o "reducedcm_"
 cp -fv ../LUTsCM2/wires.dat ../LUTsCM2/memorymodules.dat ../LUTsCM2/processingmodules.dat ./
