e8e014c49156b31e9bbabe950ffb1218c7f7013c
Anders Ryd <ar322@cornell.edu>
2024-03-23 15:37:48 -0400

    New tag or project_generation_scripts and fix for generate_MP.py

--- emData/generate_MP.py
+++ emData/generate_MP.py
@@ -80,7 +80,7 @@ with open(arguments.wiresFileName) as wiresFile:
         line = line.rstrip()
         mpName = re.sub(r".*MP_(......).*", r"MP_\1", line)
         memName = line.split()[0]
-        if memName.startswith("TPROJ_"):
+        if memName.startswith("MPROJ_"):
             if mpName not in TPMems:
                 TPMems[mpName] = []
             TPMems[mpName].append(memName)
@@ -141,7 +141,17 @@ with open(os.path.join(dirname, arguments.outputDirectory, "MatchProcessor_param
         FMMask = 0
         for FM in FMMems[mpName]:
             FMMask = FMMask | (1 << TF_index[FM])
-        
+
+        NPage = 0
+        NPageSum = 0
+        index = 0
+        for TPROJ in TPMems[mpName]:
+            npage = len(TPROJ)-17
+            NPageSum += npage
+            print("TPROJ npage", TPROJ, npage)
+            NPage = NPage | (npage << (3*index))
+            index+=1
+            
         maxTPMems += seed + "PHI" + iMP + "maxTrackletProjections"
         maxFMMems += seed + "PHI" + iMP + "maxFullMatchCopies"
         if mpName != sorted(TPMems.keys(), key=lambda x: x.startswith('L'))[-1]:
@@ -219,6 +229,8 @@ with open(os.path.join(dirname, arguments.outputDirectory, "MatchProcessor_param
         topFile.write(
             "#pragma HLS resource variable=allstub->get_mem() latency=2\n"
             "#pragma HLS resource variable=instubdata.get_mem() latency=2\n"
+            "#pragma HLS resource variable=instubdata.get_mem_entries8A() latency=1\n"
+            "#pragma HLS resource variable=instubdata.get_mem_entries8B() latency=1\n"
            "\n"
             "MP_" + seed + "PHI" + iMP + ": MatchProcessor<"
             "" + TProjRegion + ", " + VMStubRegion + ", " + nrz + ", " + VMProjRegion + ", "  + ASRegion(seed) + ", " + FMRegion(seed) + ", " + seed + "PHI" + iMP + "maxTrackletProjections" + ", " + seed + "PHI" + iMP + "maxFullMatchCopies" + ",\n"
