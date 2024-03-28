c39275ec5be75cac3353d01788c6ae61ba62b48a
Peter Wittich <wittich@cornell.edu>
2024-03-28 09:45:42 -0400

    update to tags on emp-fwk and CM_FPGA_FW

--- IntegrationTests/DualFPGA/firmware/scripts/setup_project.sh
+++ IntegrationTests/DualFPGA/firmware/scripts/setup_project.sh
@@ -8,8 +8,8 @@ PROJ_DIR=dual_fpga
 [ -d ${PROJ_DIR} ] && rm -rf ${PROJ_DIR}
 ipbb init ${PROJ_DIR}
 cd ${PROJ_DIR}
-ipbb -e add git ssh://git@gitlab.cern.ch:7999/p2-xware/firmware/emp-fwk.git -b apollo_fpga2 
-ipbb -e add git https://github.com/apollo-lhc/CM_FPGA_FW -b merge/waiting_on_emp
+ipbb -e add git ssh://git@gitlab.cern.ch:7999/p2-xware/firmware/emp-fwk.git -b v0.9.0-alpha1
+ipbb -e add git https://github.com/apollo-lhc/CM_FPGA_FW -b v3.0.0
 
 
 ipbb add git https://gitlab.cern.ch/ttc/legacy_ttc.git -b v2.1
@@ -37,5 +37,3 @@ cd proj/tf_f2/
 ipbb ipbus gendecoders
 ipbb vivado generate-project --single
 ipbb vivado synth -j16 impl -j16 package
-
-
