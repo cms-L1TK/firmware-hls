fa625f35622f5295dc34d42f212bbfe42c65e8d0
Jason Fan <jasonfan393@gmail.com>
2024-06-06 11:53:08 -0400

    vhdl cleanups

--- IntegrationTests/common/hdl/tf_merge_streamer.vhd
+++ IntegrationTests/common/hdl/tf_merge_streamer.vhd
@@ -1,7 +1,7 @@
 --===========================================================================
 --! @file
---! @brief Module which reads and streams out the contents of the memories at 
---! the end of the first half of the TF algo.
+--! @brief Module which reads and streams out the contents of the memories
+--! at the end of the first half of the TF algo.
 --! @author Jason Fan (jf847@cornell.edu)
 --! @date 2024-02-29
 --! @version v.1.0
@@ -37,10 +37,16 @@ entity tf_merge_streamer is
 	bx_in : in std_logic_vector(2 downto 0 );
 	rst: in std_logic;
 	clk : in std_logic;
-	enb_arr: out std_logic_vector(NUM_INPUTS-1 downto 0); -- output read enable
+  --output read enable to tf_mem modules
+	enb_arr: out std_logic_vector(NUM_INPUTS-1 downto 0);
 	bx_out : out std_logic_vector(2 downto 0);
-	merged_dout : out std_logic_vector(RAM_WIDTH+NUM_EXTRA_BITS downto 0); --output stream of merged memories with a valid bit
-	din0: in std_logic_vector(RAM_WIDTH-1 downto 0); --din/nent should be arrays but Vivado sim does not support unconstrained array
+  --output merged stream, includes input word, up to 2 bits that encode the
+  --original module, and a valid bit (from LSB to MSB)
+	merged_dout : out std_logic_vector(RAM_WIDTH+NUM_EXTRA_BITS downto 0);
+  --input data,nent and addresses are best suited for unconstrained arrays
+  --but this is not supported in vivado 2019
+  --module always accepts 4 input memories, but will not use all of them
+	din0: in std_logic_vector(RAM_WIDTH-1 downto 0);
 	din1: in std_logic_vector(RAM_WIDTH-1 downto 0);
 	din2: in std_logic_vector(RAM_WIDTH-1 downto 0);
 	din3: in std_logic_vector(RAM_WIDTH-1 downto 0);
@@ -48,17 +54,25 @@ entity tf_merge_streamer is
 	nent1: in t_arr_7b(0 to NUM_PAGES-1);
 	nent2: in t_arr_7b(0 to NUM_PAGES-1);
 	nent3: in t_arr_7b(0 to NUM_PAGES-1);
-	addr_arr: out std_logic_vector(NUM_INPUTS*CLOGB2(RAM_DEPTH)-1 downto 0) -- this should be an array but Vivado does not support unconstrained arrays
+	addr_arr: out std_logic_vector(NUM_INPUTS*CLOGB2(RAM_DEPTH)-1 downto 0)
 ) ;
 end entity tf_merge_streamer;
 
 architecture RTL of tf_merge_streamer is
-	type mem_count_arr is array(0 to NUM_INPUTS-1) of integer;
-	type nent_array is array(4-1 downto 0) of t_arr_7b(0 to NUM_PAGES-1);
-	type din_array is array(4-1 downto 0) of std_logic_vector(RAM_WIDTH-1 downto 0);
 
-	signal valid : std_logic_vector(3 downto 0) := (others => '0');
-	signal readmask : std_logic_vector(NUM_INPUTS-1 downto 0) := (others => '0'); --tracks which inputs have an available word to read
+  constant MAX_INPUTS : integer := 4;
+  constant pipe_stages : integer := 4;
+
+	type mem_count_arr is array(NUM_INPUTS-1 downto 0) of integer;
+	type toread_arr is array(pipe_stages-1 downto 0) of integer;
+
+  --nent and din are repackaged from odd input type into
+  --arrays
+	type nent_array is array(MAX_INPUTS-1 downto 0) of t_arr_7b(0 to NUM_PAGES-1);
+	type din_array is array(MAX_INPUTS-1 downto 0) of std_logic_vector(RAM_WIDTH-1 downto 0);
+
+	signal valid : std_logic_vector(pipe_stages-1 downto 0) := (others => '0');
+	signal readmask : std_logic_vector(NUM_INPUTS-1 downto 0) := (others => '0');
 
 begin
 	process(clk)
@@ -69,10 +83,7 @@ begin
 	variable current_page: natural := 0;
 	variable bx_change : boolean := false; -- indicates to the module whether or not the bx has changed compared to the previous clock
 
-	variable toread : integer := 0;
-	variable toread1 : integer := 0;
-	variable toread2 : integer := 0;
-	variable toread3 : integer := 0;
+	variable toread : toread_arr := (others => 0);
 
 	begin
     if rising_edge(clk) then
@@ -82,7 +93,6 @@ begin
 			if (bx_change) then --reset with rst signal or a change in bx
 			-- check if bx changes and update page to read from
 				mem_count := (others => 0);
-				report "BX CHANGE";
 			end if ;
 			current_page := to_integer(unsigned(bx_in)) mod NUM_PAGES;
      --check if memory read counter is less than nentries
@@ -97,33 +107,31 @@ begin
 			    valid(0) <= '0';
 			else
 			    for j in 0 to NUM_INPUTS-1 loop
-			        if readmask((j + toread + 1) mod NUM_INPUTS) = '1' then
-			            toread := (j + toread + 1 ) mod NUM_INPUTS;
+			        if readmask((j + toread(0) + 1) mod NUM_INPUTS) = '1' then
+			            toread(0) := (j + toread(0) + 1 ) mod NUM_INPUTS;
 			            exit;
 			        end if;
 			    end loop;
-			    addr_arr(((toread+1)*clogb2(RAM_DEPTH))-1 downto (toread)*clogb2(RAM_DEPTH)) <= std_logic_vector(to_unsigned(current_page*page_length + mem_count(toread), clogb2(RAM_DEPTH)));
+			    addr_arr(((toread(0)+1)*clogb2(RAM_DEPTH))-1 downto (toread(0))*clogb2(RAM_DEPTH)) <= std_logic_vector(to_unsigned(current_page*page_length + mem_count(toread(0)), clogb2(RAM_DEPTH)));
                 valid(0) <= '1';
-                mem_count(toread) := mem_count(toread) + 1;
+                mem_count(toread(0)) := mem_count(toread(0)) + 1;
 			end if;
 
-			if valid(3) ='1' then
+			if valid(pipe_stages-1) ='1' then
 			     if (NUM_EXTRA_BITS > 0) then
-			         merged_dout <= '1' & std_logic_vector(to_unsigned(toread3,NUM_EXTRA_BITS)) & din_arr(toread3);
+			         merged_dout <= '1' & std_logic_vector(to_unsigned(toread(pipe_stages-1),NUM_EXTRA_BITS)) & din_arr(toread(pipe_stages-1));
 			     else
-			         merged_dout <= '1' & din_arr(toread2);
+			         merged_dout <= '1' & din_arr(toread(pipe_stages-2));
 			     end if ;
 			else
 			     merged_dout <= (others => '0');
 			end if;
 			bx_last := to_integer(unsigned(bx_in));
-			bx_out <= bx_in; -- FIXME do i need this?
-			toread3 := toread2;
-			toread2 := toread1;
-			toread1 := toread;
-			valid(1) <= valid(0);
-			valid(2) <= valid(1);
-			valid(3) <= valid(2);
+			bx_out <= bx_in;
+      for j in 0 to pipe_stages-2 loop
+	  		toread(j) := toread(j+1);
+  			valid(j) <= valid(j+1);
+      end loop;
 		end if;
 	end process;
 end RTL;
