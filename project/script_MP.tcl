c8e770929069dc751af68e9c2e9caa2c2267de2a
Anders Ryd <ar322@cornell.edu>
2024-07-30 12:33:50 -0400

    Some simplification in the MP to reduce resoureces

--- project/script_MP.tcl
+++ project/script_MP.tcl
@@ -28,7 +28,7 @@ set modules_to_test {
 
 # module_to_export must correspond to the default macros set at the top of the
 # test bench; otherwise, the C/RTL cosimulation will fail
-set module_to_export MP_L1PHIC
+set module_to_export MP_D1PHIC
 
 # create new project (deleting any existing one of same name)
 open_project -reset match_processor
