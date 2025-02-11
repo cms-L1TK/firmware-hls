4772efa11a77165dc95c956b068d7ad8f3deda99
Brent Yates <brentRyates@gmail.com>
2025-02-11 09:37:06 -0500

    Remove `static const` and add `initialized`

--- TrackletAlgorithm/MatchEngineUnit_parameters.h
+++ TrackletAlgorithm/MatchEngineUnit_parameters.h
@@ -10,13 +10,13 @@ static const ap_uint<(1 << (2 * kNBitsBuffer))> nearFullUnit() {
   static ap_uint<(1 << (2 * kNBitsBuffer))> lut;
   for(int i = 0; i < (1 << (2 * kNBitsBuffer)); ++i) {
 #pragma HLS unroll
-    ap_uint<kNBitsBuffer> wptr, rptr;
-    ap_uint<2 * kNBitsBuffer> address(i);
-    (rptr,wptr) = address;
-    auto wptr1 = wptr+1;
-    auto wptr2 = wptr+2;
-    bool result = wptr1==rptr || wptr2==rptr;
-    lut[i] = result;
+   ap_uint<kNBitsBuffer> wptr, rptr;
+   ap_uint<2 * kNBitsBuffer> address(i);
+   (rptr,wptr) = address;
+   auto wptr1 = wptr+1;
+   auto wptr2 = wptr+2;
+   bool result = wptr1==rptr || wptr2==rptr;
+   lut[i] = result;
   }
   return lut;
 }
@@ -29,18 +29,22 @@ static bool nearFullUnitBool(ap_uint<kNBitsBuffer> rptr, ap_uint<kNBitsBuffer> w
 }
 
 template<int kNBitsBuffer>
-static const ap_uint<(1 << (2 * kNBitsBuffer))> nearFull3Unit() {
+const ap_uint<(1 << (2 * kNBitsBuffer))> nearFull3Unit() {
   static ap_uint<(1 << (2 * kNBitsBuffer))> lut;
-  for(int i = 0; i < (1 << (2 * kNBitsBuffer)); ++i) {
+  static bool initialized = false;
+  if(!initialized) {
+    initialized = true;
+    for(int i = 0; i < (1 << (2 * kNBitsBuffer)); ++i) {
 #pragma HLS unroll
-    ap_uint<kNBitsBuffer> wptr, rptr;
-    ap_uint<2 * kNBitsBuffer> address(i);
-    (rptr,wptr) = address;
-    ap_uint<kNBitsBuffer> wptr1 = wptr+1;
-    ap_uint<kNBitsBuffer> wptr2 = wptr+2;
-    ap_uint<kNBitsBuffer> wptr3 = wptr+3;
-    bool result = wptr1==rptr || wptr2==rptr || wptr3==rptr;
-    lut[i] = result;
+      ap_uint<kNBitsBuffer> wptr, rptr;
+      ap_uint<2 * kNBitsBuffer> address(i);
+      (rptr,wptr) = address;
+      ap_uint<kNBitsBuffer> wptr1 = wptr+1;
+      ap_uint<kNBitsBuffer> wptr2 = wptr+2;
+      ap_uint<kNBitsBuffer> wptr3 = wptr+3;
+      bool result = wptr1==rptr || wptr2==rptr || wptr3==rptr;
+      lut[i] = result;
+    }
   }
   return lut;
 }
@@ -81,15 +85,19 @@ static bool nearFull4UnitBool(ap_uint<kNBitsBuffer> rptr, ap_uint<kNBitsBuffer> 
 }
 
 template<int kNBitsBuffer>
-static const ap_uint<(1 << (2 * kNBitsBuffer))> emptyUnit() {
+const ap_uint<(1 << (2 * kNBitsBuffer))> emptyUnit() {
   static ap_uint<(1 << (2 * kNBitsBuffer))> lut;
-  for(int i = 0; i < (1 << (2 * kNBitsBuffer)); ++i) {
+  static bool initialized = false;
+  if(!initialized) {
+    initialized = true;
+    for(int i = 0; i < (1 << (2 * kNBitsBuffer)); ++i) {
 #pragma HLS unroll
-    ap_uint<kNBitsBuffer> wptr, rptr;
-    ap_uint<2 * kNBitsBuffer> address(i);
-    (rptr,wptr) = address;
-    bool result = wptr==rptr;
-    lut[i] = result;
+      ap_uint<kNBitsBuffer> wptr, rptr;
+      ap_uint<2 * kNBitsBuffer> address(i);
+      (rptr,wptr) = address;
+      bool result = wptr==rptr;
+      lut[i] = result;
+    }
   }
   return lut;
 }
