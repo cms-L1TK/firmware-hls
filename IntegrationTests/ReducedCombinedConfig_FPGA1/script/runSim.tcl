70873a24b2781607a25078019cb03b9156049b9f
Anders Ryd <ar322@cornell.edu>
2025-04-24 14:50:00 -0400

    Add reduced FPGA1 project to CI build

---
+++ IntegrationTests/ReducedCombinedConfig_FPGA1/script/runSim.tcl
@@ -0,0 +1,19 @@
+# Open project
+set projName "Work"
+open_project $projName/$projName.xpr
+
+reset_simulation sim_1
+
+# Create directory for output .txt file
+file delete -force dataOut/
+file mkdir dataOut/
+
+# Launch Simulation
+launch_simulation
+
+# Set default wave viewer cfg
+open_wave_config {../tb/start_bx.wcfg}
+
+restart
+# Need 4us + 0.45us per event (50us for 100 events, but 10us for quick test).
+run 50 us
