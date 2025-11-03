40865a40b056a1b0f851c3c8368eb885f8195cef
Andrew Hart <ahart@cern.ch>
2025-11-03 09:27:20 -0500

    Updated best implementation strategy.

--- IntegrationTests/DualFPGA/firmware/ucf/synth_impl_settings.tcl
+++ IntegrationTests/DualFPGA/firmware/ucf/synth_impl_settings.tcl
@@ -1,5 +1,1 @@
-set_property strategy Flow_PerfOptimized_high [get_runs synth_1] [get_runs synth_1]
-set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
-set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
-set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AlternateReplication [get_runs impl_1]
-
+set_property strategy Performance_Auto_1 [get_runs impl_1]
